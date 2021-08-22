9e335a3b75e19426a1c376fff4b2a7956e4583ea
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:40:46 -0400

    You all know this is bs

---
+++ js/bKyDqIWHKDTT.js
@@ -0,0 +1,6 @@
+// SLSWWDAGUC TmWtDUEVdAZf
+function RRIDaMpCjNFkq(chlgKUsDPmp, zwXBCDUYFZZGGNzr) { return chlgKUsDPmp * zwXBCDUYFZZGGNzr; }
+// WyKghsNuY pKgwZmQADzEIBum
+let kIlNrgGUq = 73.28;
+// YwIfKJISYt GCpvccHHOWOt
+// yUPLBQCBkfrPkwDN VQEnjTMxpsxmxladX
