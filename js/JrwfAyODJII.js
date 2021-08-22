d4c377d4c43c4f693b7926fa65fdb786ba22b97f
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:44:13 -0400

    Typos

---
+++ js/JrwfAyODJII.js
@@ -0,0 +1,4 @@
+function ZHYrECcin(BMVQoQIwhM, mIuHbKiyFik) { return BMVQoQIwhM * mIuHbKiyFik; }
+let OoBrklvUjDL = 135.38;
+function uCiZIJfshCTX(WOZEDrLrStjZgYJ, YFgDsVvQvspRXBLH) { return WOZEDrLrStjZgYJ * YFgDsVvQvspRXBLH; }
+// UPccaPMvqLbMhU AzBqooiHuTLWdg
