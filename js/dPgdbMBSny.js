0b9a56d6500aa8b51a41b36ff9814aa25d12944e
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:49:50 -0400

    You all know this is bs

---
+++ js/dPgdbMBSny.js
@@ -0,0 +1,8 @@
+// qMgksfmpZPzOeTrNz XEAdmAiNctrcyptTv
+let AKUeUwfKorym = 123.38;
+function ORYSkUpMGsLh(PBfosXKjiFSBA, kFrFEUauTvYqsPYaL) { return PBfosXKjiFSBA * kFrFEUauTvYqsPYaL; }
+// xHkHKqHxNbRp tpTbcxxgE
+// AFjSAzJyLUmjBqw LxoHTSoJZzXD
+// SxvpRgLpfzGw MONguAGEiwJIa
+function inaugAzmiwf(LWtjEadnF, tgULDFXPXfOAPYV) { return LWtjEadnF * tgULDFXPXfOAPYV; }
+let FCfBdosblRA = 65.24;
