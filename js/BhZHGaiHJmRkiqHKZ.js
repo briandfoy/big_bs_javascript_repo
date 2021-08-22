91567db1ac2cd9849d334a473fbcd6f0393e7f2f
brian d foy <brian.d.foy@gmail.com>
2021-08-22 06:15:31 -0400

    Re-order to make more sense

---
+++ js/BhZHGaiHJmRkiqHKZ.js
@@ -0,0 +1,6 @@
+function rLBwvSJOq(gdLhNwLqUiUr, aKsjWwHrYNPn) { return gdLhNwLqUiUr * aKsjWwHrYNPn; }
+function BnxdRRnbOUza(dBVlWfAEYhTHyWb, XpXyHZGTtpBK) { return dBVlWfAEYhTHyWb * XpXyHZGTtpBK; }
+// EmrziWPxXiadu bhZpcyooHtywowgQ
+function kgPVseQfPa(lmeFwZQwh, uYwatFfEXWaj) { return lmeFwZQwh * uYwatFfEXWaj; }
+// hHhOZjFgSGFnyszO nabSTYtZTnsZ
+// MroLoCYrXfCbTJUEa blhyhtdgHUJb
