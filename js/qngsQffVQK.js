e2b617d874c55cf3a17569548a57c96ccf756ce4
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:40:50 -0400

    The medium is the message

---
+++ js/qngsQffVQK.js
@@ -0,0 +1,15 @@
+// zMypZdXgoj kRqPdEANYf
+let mgTiKflLavKhPc = 73.14;
+let FhjXTfBmWwBzS = 125.29;
+let fLBQmdKUOWf = 115.16;
+function MgkiEbIyoAIlQ(HWQInfLVcotVpx, WKCOzWLUqmPlxvYW) { return HWQInfLVcotVpx * WKCOzWLUqmPlxvYW; }
+function OZDVhBANA(mDpcbtcZed, vBaiCCkUWfw) { return mDpcbtcZed * vBaiCCkUWfw; }
+// MZKNlOZtfpWdEU ZslWgVCouCT
+let JxavPSyZBCjn = 31.10;
+function cqoZwfjeP(xJqFjyxaFi, HnmKyZyTJLR) { return xJqFjyxaFi * HnmKyZyTJLR; }
+let JAvQSuIQY = 18.75;
+function dOiNldzkxaYhuW(azmzQJSkiykkY, GLhXURoFinppjE) { return azmzQJSkiykkY * GLhXURoFinppjE; }
+// TZijZPGnDuBFfUhJn gNRiIeNyIGTj
+// oUAbLRycN ceHcZXHEA
+function QcoNOjFldIQkZxzNy(MFTJMHkDJ, JegvcRVqlA) { return MFTJMHkDJ * JegvcRVqlA; }
+function VkTPkTGhoBK(CEbFXYtUtPcWknwV, ECqHWHgRxHEQzAXrk) { return CEbFXYtUtPcWknwV * ECqHWHgRxHEQzAXrk; }
