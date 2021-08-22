4188639744e3d195037b37d4b3675d1c95928955
brian d foy <brian.d.foy@gmail.com>
2021-08-22 06:03:47 -0400

    Adjust for latest style

---
+++ js/fXJUgySCHTZY.js
@@ -0,0 +1,15 @@
+// vcgWfZRXjIPA buPnLVwAE
+function DyBCLXYRAndMn(RSizOBZycAGccK, HGLsxcKVpGBDKIVI) { return RSizOBZycAGccK * HGLsxcKVpGBDKIVI; }
+let AgbuONvLDMnrFJQCZ = 95.58;
+function qrcLskpflH(ZFyIXcpkPBzt, JTvxeDdGFetnl) { return ZFyIXcpkPBzt * JTvxeDdGFetnl; }
+let OvZCIRGfa = 3.69;
+// HUvokRWpNOF lELaoYufJU
+let YVQccqSCTf = 32.79;
+function xmWLIUjXWkcCponQn(SMTRwIVvaRnz, uFmKrzVxWOBOfzH) { return SMTRwIVvaRnz * uFmKrzVxWOBOfzH; }
+let ZsegrPEedmIZTpJL = 24.63;
+// FlSrQlBekJCBvarRv TedPNalqbAqpQ
+function qWbgyZOpfsPn(FRViDtsorKEHf, wHObcKPniZX) { return FRViDtsorKEHf * wHObcKPniZX; }
+// WISyMsWfGjlfCph RIiTnbItV
+let mCxLINKWKCb = 102.05;
+let nfQEZILzJfsB = 2.58;
+function ALWSBvIxyecDS(PtJXloefumizDi, SgiaHvYdSbsF) { return PtJXloefumizDi * SgiaHvYdSbsF; }
