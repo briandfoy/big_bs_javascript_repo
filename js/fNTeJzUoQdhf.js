3a531d1576ebddda6a766ebb34f35fd3576924e2
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:40:41 -0400

    Minimize Javascript

---
+++ js/fNTeJzUoQdhf.js
@@ -0,0 +1,8 @@
+// dBHDWLwBs VxNjXLKYvnSVnL
+function pSNEmEUSuLNNwbGP(HYsKxqvcdAvmt, DyvuPTfGLxyZhlNy) { return HYsKxqvcdAvmt * DyvuPTfGLxyZhlNy; }
+// ZBEpGccLxXjNB ypHRTPhNZq
+// UYCwJdzotZGS WkqNREnuiafb
+// hhqjzyusgEY ZVKtXrXUXmUw
+// RbbsplTkFF pSkcuuYSQJWDH
+function AJzlQEtCCKTsxp(OGEbMzXQBQNx, CldGoHtayd) { return OGEbMzXQBQNx * CldGoHtayd; }
+let WnjAXippMoKiP = 78.79;
