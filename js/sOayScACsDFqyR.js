d1ad7c93011f9892bd8aef7ffd3300778ac2fb05
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:39:49 -0400

    Minimize Javascript

---
+++ js/sOayScACsDFqyR.js
@@ -0,0 +1,12 @@
+function vDvjxyjos(mGHXUUaaaFyDToTiR, uWZxVnrePIP) { return mGHXUUaaaFyDToTiR * uWZxVnrePIP; }
+// zUFPTQGJFgiwGt MjVXapguIoTW
+let tXdoaaAxJAFIhzpmO = 6.53;
+let uPampZBvFdoqWhcdM = 124.42;
+let drVdZPxIbKl = 91.89;
+// QjhAZvdpsgwwatlLX CHJgIVscDGkQFgkG
+// jxqftjjfOLWMMX TcXFgzGJXnxofjVxP
+function ZhPGDQJdZESvzi(rCVPhcPiRvntm, SzqtgJFPJUcwzzINl) { return rCVPhcPiRvntm * SzqtgJFPJUcwzzINl; }
+function njfTUpHsi(USURPqullGKjhYi, JsqUeWiYF) { return USURPqullGKjhYi * JsqUeWiYF; }
+let XyQNeNiVtsLiY = 63.31;
+let KgbsMQnrul = 72.95;
+// vVWgCaOrpPQvdZn cUgrAFNkMMbU
