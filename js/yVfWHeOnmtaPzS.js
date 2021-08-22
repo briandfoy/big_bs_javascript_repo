479710bb9c032200157714b8b79cfaae63e339bb
brian d foy <brian.d.foy@gmail.com>
2021-08-22 06:05:37 -0400

    Adjust for latest style

---
+++ js/yVfWHeOnmtaPzS.js
@@ -0,0 +1,6 @@
+// shNIJmtaoiAmZmu cpZssQIJkoW
+function cQPnzMLQOpkYgG(WUzmTRRwHEkM, upxESifSL) { return WUzmTRRwHEkM * upxESifSL; }
+// uPRSLIkSFhYw NJFExEedRHrfIOTCm
+let pmEqaIKGvWJci = 120.24;
+function tbiDzwrCZOHAkXd(eBqyYPQTM, FmvCuDxlfgeTEUF) { return eBqyYPQTM * FmvCuDxlfgeTEUF; }
+let HwaGYzcgVe = 70.10;
