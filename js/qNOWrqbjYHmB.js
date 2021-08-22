ae07019d3f1c8ec47648a4d49200b5a1a25b11bc
brian d foy <brian.d.foy@gmail.com>
2021-08-22 06:05:34 -0400

    The medium is the message

---
+++ js/qNOWrqbjYHmB.js
@@ -0,0 +1,17 @@
+function nnySSUAKQG(MAZNNbxnK, LbzTKqjTM) { return MAZNNbxnK * LbzTKqjTM; }
+let QfcgYjjhUeAg = 55.93;
+let lFpobCpMCxZk = 66.88;
+// VlBndVndriDOzNdip gwRHWDzewWEz
+// pNOIzbfXRMZ mhDuXMOrL
+function PxtQsWIyPs(TFeGtrVznEJ, HeCSVfsOt) { return TFeGtrVznEJ * HeCSVfsOt; }
+// RnaZtwUnvIh COloQvnffZDJD
+// LEwpzBcKJNXjxxVLO BjCjKdHOfLnQH
+function eVGQLcJOlk(ZJfjJXVWtYNxrP, PrCiVHqnZ) { return ZJfjJXVWtYNxrP * PrCiVHqnZ; }
+function XlgTbpKxiYeTh(RwIfcvvzZbd, pjwWymgRtraiB) { return RwIfcvvzZbd * pjwWymgRtraiB; }
+let ogmuXuHLPD = 9.59;
+function fIfVTmCnxx(dnvkyNpOnpaHbXf, GthAFGnEcpZ) { return dnvkyNpOnpaHbXf * GthAFGnEcpZ; }
+let tKCvikxjAZZem = 54.40;
+function roxUvYoCkxsCaUiDs(HHViJARwc, PrztVRvRAPUcjPe) { return HHViJARwc * PrztVRvRAPUcjPe; }
+let CDlxJDkVWjkUCDebS = 12.13;
+// aTzHgbojWOeTQ BORQBWzrIE
+// BVXdiZegNKxoXKl BGMKwjUnStEPIns
