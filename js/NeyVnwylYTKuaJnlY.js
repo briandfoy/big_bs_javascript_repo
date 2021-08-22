8017ac022bad63a92c9b083dd84ef1899bf1872e
brian d foy <brian.d.foy@gmail.com>
2021-08-22 06:03:47 -0400

    Re-order to make more sense

---
+++ js/NeyVnwylYTKuaJnlY.js
@@ -0,0 +1,5 @@
+let wPuMMjRyDz = 33.62;
+// znoDkcSksalh TmjWghdnsBWoE
+function SsolxsNxOutupJySW(jUiAwrqEV, XNOkEJJDfcTqgmm) { return jUiAwrqEV * XNOkEJJDfcTqgmm; }
+// mpmbrVHmtCNEn grxIJjSTDp
+function bFazngMaJ(dbFILdmeBXYiOmgC, cKbYBovFdWkz) { return dbFILdmeBXYiOmgC * cKbYBovFdWkz; }
