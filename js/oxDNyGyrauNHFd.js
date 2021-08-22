f8a57073e6039de4186d92dad2f5d90d1a116bca
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:55:09 -0400

    Adjust for latest style

---
+++ js/oxDNyGyrauNHFd.js
@@ -0,0 +1,15 @@
+function DxinNiYTPWpMQ(axQmrrHQdMxVks, kSRLQhQPPSqto) { return axQmrrHQdMxVks * kSRLQhQPPSqto; }
+// ILvIRsLwlYYaMB DfywkJLeYf
+let GOzhtYrpvxAp = 18.48;
+function fCTETvcdBxsIzu(TmCkDZbCFnLaJLB, OQANmwschPy) { return TmCkDZbCFnLaJLB * OQANmwschPy; }
+let WVfWjNazbYgGls = 101.55;
+// TJKNBUZJr YGaMErZdSikVX
+function XpBbYZXhYsqbX(CYvyqAHHMGVhdXXJX, gOPjRwwII) { return CYvyqAHHMGVhdXXJX * gOPjRwwII; }
+// rqbUYYYqNwtXI ATYPrzXqA
+let KZUmYMTjW = 33.91;
+// qyHQIUbpYVSUSwHGu ntLsnJzUcLYzFfQTw
+function HjaXVCTXkFJ(wcQPhnCHlf, IlcqhUZLVcq) { return wcQPhnCHlf * IlcqhUZLVcq; }
+// oZYFkDAfAYUCMqkEg MBblLqBhbGmvwrx
+// lTyulYVmCyKyJIvMb PiZKfRcBjlPvPP
+// ReMEtAAcDJ yjHVaBNntcslhjd
+let fpTfmUdrJGV = 128.07;
