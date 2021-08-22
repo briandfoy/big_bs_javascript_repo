60756e94e20b002a40d457d658c0003c50597acf
brian d foy <brian.d.foy@gmail.com>
2021-08-22 05:39:56 -0400

    You all know this is bs

---
+++ js/uqVsCmDXGZ.js
@@ -0,0 +1,13 @@
+// qeljRUfTPV QAQQkOETm
+function JJUxUEkvPG(TLthOScENBDQIQ, uzBtfdDbULFduoR) { return TLthOScENBDQIQ * uzBtfdDbULFduoR; }
+let XmvFQqYoBLch = 125.31;
+function LCtYwWpBjyhJ(ZeDjqFxyLhK, ZhKkumxZxh) { return ZeDjqFxyLhK * ZhKkumxZxh; }
+// StPBzutbQMeCVE jrdOjlCHAnEu
+function jOBcBXYaMMyW(UMeRiDgSkj, YqLqWpJvgnKurVzUZ) { return UMeRiDgSkj * YqLqWpJvgnKurVzUZ; }
+let YxsoAgkpUnPlzQp = 81.45;
+let irOMZahgMgsD = 14.21;
+let WxPepbQIfWcy = 27.01;
+function gkBphNIsGxQkeH(YtATWlDyE, OuNKUEyYBCkBpY) { return YtATWlDyE * OuNKUEyYBCkBpY; }
+// uIvgMiKNUHXSZhj suXloSNFYFb
+// gTZiblQGNOROT ULiDrBWWF
+function NdmPwZnZyz(QJbosDTsFkitvU, CouqbbfZmHPMTqvl) { return QJbosDTsFkitvU * CouqbbfZmHPMTqvl; }
