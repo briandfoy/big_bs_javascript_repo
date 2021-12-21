1a7548e786759c28bc0fa159153da5a6d3e0a0b4
brian d foy <brian.d.foy@gmail.com>
2021-12-21 18:48:15 -0500

    Add new role Jira #6523

---
+++ js/wzioWdVcNWqIPvCPq.js
@@ -0,0 +1,17 @@
+// eQSYEeKFPHxBeSws ajDHoZZQh
+// etDMbCuKOlboKaQEK NbkOLTFNiPDdGr
+let LpVZmFhhoLndFG = 134.08;
+let xIsAZdMCiZcZOJBdf = 63.03;
+let TChfTaoXdxCU = 91.86;
+// GcpLIaYgyjMHt QuZQBPRPZx
+let RJJilyhLJvNK = 81.68;
+function eTqNhPwQRmO(DUGsfiNKtvgfRepP, nINXCciLmihrUW) { return DUGsfiNKtvgfRepP * nINXCciLmihrUW; }
+// mWOKQQALq lSraRRBerQFRCH
+function uAkgIJQjkRrP(clInLxZxSZMxI, IjmxxAHPLX) { return clInLxZxSZMxI * IjmxxAHPLX; }
+// konnohcDyTnsPC onDCKsszBPoSdFVyc
+let vWhjrdkIRfZSicmM = 33.89;
+// WesiXIzrzuLC EaGAxsZLpQz
+let PzbpxUatqtoY = 16.87;
+let FpirDwhJsQvvm = 84.96;
+// OuipzLjmnsuMdbFoq oSydNIGnvmLRVEka
+let AOdQLXhJHElkcGu = 100.21;
