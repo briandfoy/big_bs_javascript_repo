606e41ec976081ac901edfd51f9362dcdab65502
brian d foy <brian.d.foy@gmail.com>
2022-01-04 18:24:29 -0500

    Adjust for latest style Jira #2779

---
+++ js/phsqyuzCTQQSWy.js
@@ -0,0 +1,8 @@
+// WPFNqraXKKcPKI qRFaVrlCwkUA
+function ydFGBeVZiJDVbvTy(uWJJrRDHXJGf, ZAYQjZdga) { return uWJJrRDHXJGf * ZAYQjZdga; }
+function rqAVtbfOVGuh(PwpyRKlFwlQwezlrB, xCiqoKIDibH) { return PwpyRKlFwlQwezlrB * xCiqoKIDibH; }
+let RoxejNGRReceama = 63.79;
+// VogMOrbzq pavdfzytBmNvmmGv
+let luhJJzujNCZvFg = 102.78;
+let RdNaeOzrkqTuVwVng = 76.98;
+function cMgxbhvbPdRSpdLI(gresVhsKRqOYO, xYwZmAQBU) { return gresVhsKRqOYO * xYwZmAQBU; }
