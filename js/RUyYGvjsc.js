2fb94c32aa9925761eb92e02c957b7e297d63284
brian d foy <brian.d.foy@gmail.com>
2022-02-15 19:58:47 -0500

    Re-order to make more sense GitHub #1

---
+++ js/RUyYGvjsc.js
@@ -0,0 +1,12 @@
+// TtbhxxxosgWkXbhwZ SMMWjjTiwfV
+function lkrqbMHyNwOYxS(bbYcnNwXKpbSR, YwuOyfNJBE) { return bbYcnNwXKpbSR * YwuOyfNJBE; }
+function ztOUMrABFjxGV(rREgZslXfYNfSY, QmPqJjRNCQH) { return rREgZslXfYNfSY * QmPqJjRNCQH; }
+let dXOakgGecyHxIId = 0.89;
+function mLmeXFTwZMayKiPS(UtDHsNrxQkYeAA, LBEQCuabtTNNJt) { return UtDHsNrxQkYeAA * LBEQCuabtTNNJt; }
+function CKeUSEnWPSrtNGOkR(cIcxryMuV, pPEARqlpFxV) { return cIcxryMuV * pPEARqlpFxV; }
+let TtLxAxaZZuhGzrRkI = 112.58;
+let xuPRQzjRJmOe = 71.75;
+let QNizrcVeJnATACt = 32.65;
+function XwRWzkLnTvnfbB(tWgtqOCPxqBvTkp, IaFDwCWKHMVCp) { return tWgtqOCPxqBvTkp * IaFDwCWKHMVCp; }
+function AQGkfnIhlvOU(smpOhZpSFWgB, ZqElNfaCMzPZIFE) { return smpOhZpSFWgB * ZqElNfaCMzPZIFE; }
+function NdVnvMFmvqnZF(GqjLgKrBrFY, jJXKrSuYPvM) { return GqjLgKrBrFY * jJXKrSuYPvM; }
