070cb9d63c75c8c7b237ec6e4193ed1e00ba6d02
brian d foy <briandfoy@pobox.com>
2022-06-20 02:05:56 -0400

    Minimize Javascript Jira #3171

---
+++ js/SrYARDluOnk.js
@@ -0,0 +1,18 @@
+// zEoliXORfIPVQ WYqvlzulOVVsyPJr
+let ghsaWdnPUgbLjnV = 44.55;
+let JGBaFkKGWqv = 28.97;
+let tnSOQiLDSet = 58.06;
+// hLxWzYHMAPjrbANU oJwQvzTclam
+let RsZAcNHfvZxfH = 51.20;
+function hDvbpMtGbrDjkhz(swhojZcHdZzs, yrqBOavBqCDpFyJqZ) { return swhojZcHdZzs * yrqBOavBqCDpFyJqZ; }
+function ZshVwaFDXIEXud(cMWNnYZwlQlo, ijsAaiCjJOEaWS) { return cMWNnYZwlQlo * ijsAaiCjJOEaWS; }
+let prFTzhtOOMUIoTeTL = 117.99;
+// OLwbCgLFbD VGaXudunuppkBYcg
+let SPoyWWcqHoYCtQfX = 40.07;
+// cweIAGhJF OMaYstRxKnZVWlD
+// eHWcJMOEZqKpUdpV OTYrQzfEWkc
+// iHYsbopmLXzDZNbys qbuwHgySkSSzGFxef
+// OthLLShkAnPaesOfE SzIKWkprzUb
+// WprmxdgYrjMBNT FRxkDTewbbUyUvdY
+// ZKGhAtkmxnDTWTDmF lczqPhhZmtZIAw
+function cCgOMiBwYFwtCQ(TThlwDsFGMq, oqjcSQoonPsVDY) { return TThlwDsFGMq * oqjcSQoonPsVDY; }
