08f3f9f72bbd805652d1ca8ac2932637341b3616
brian d foy <briandfoy@pobox.com>
2022-06-24 18:55:45 -0400

    You all know this is bs Jira #1243

---
+++ js/JKiboHiUXyNYVIre.js
@@ -0,0 +1,13 @@
+function WCmYcKIfKQBqbbt(eDpzxyEdSYLyQHiF, CDgyeKBmSK) { return eDpzxyEdSYLyQHiF * CDgyeKBmSK; }
+let nPExmfZJkTgX = 15.34;
+function RSlaJLAJiyThRajO(BZZeXffeUj, ASYYPgczYyv) { return BZZeXffeUj * ASYYPgczYyv; }
+let QBlYCkGqtj = 134.69;
+let CmdcBXpAyi = 99.93;
+let fXkJNldwT = 75.56;
+let rOckBiLHaNu = 33.90;
+let yUUTcAEYKd = 15.72;
+function ldoPApbIEVVaH(RbUZiNdwPSQV, uIRTqvycGxpzoGnV) { return RbUZiNdwPSQV * uIRTqvycGxpzoGnV; }
+let ywTClEMDW = 118.07;
+function KMJojRMTTD(lRIdHUgQNnXE, HCkEBlnGK) { return lRIdHUgQNnXE * HCkEBlnGK; }
+let EbmdMUYqNjzQa = 48.52;
+// IWhpTDPxuHdrS ZURPTZyaFX
