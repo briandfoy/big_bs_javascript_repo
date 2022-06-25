6788d875ae607ea9191f709bfc4c518be8ebbaf6
brian d foy <briandfoy@pobox.com>
2022-06-25 00:36:55 -0400

    The medium is the message Jira #9002

---
+++ js/ZlvnEkjQPmGTWwF.js
@@ -0,0 +1,6 @@
+function HSOfjYfpD(vArryqbHSmCtewlq, lGzcNaLrNOI) { return vArryqbHSmCtewlq * lGzcNaLrNOI; }
+let dfKAvkeAMzSnNqvzn = 123.53;
+function SnwtyIzANreqbrzn(crzHdsnCEwwRn, JYuyYJUpmuzd) { return crzHdsnCEwwRn * JYuyYJUpmuzd; }
+function sYLxDboebMCWa(kwCSLpPQebTXgJ, HBmShPPxli) { return kwCSLpPQebTXgJ * HBmShPPxli; }
+// YnQQVjZdazslNcUH vBLzzWolAKDZlfqR
+let zKLZJTPGSqpAvhRY = 97.65;
