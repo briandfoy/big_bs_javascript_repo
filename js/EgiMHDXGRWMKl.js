a35bd952d0dc92d24f83fb3159515644f18efd22
brian d foy <briandfoy@pobox.com>
2022-08-04 21:49:50 -0400

    Minimize Javascript Jira #4896

---
+++ js/EgiMHDXGRWMKl.js
@@ -0,0 +1,4 @@
+// nBXvXXxxa ZNNOyZOOTqEjNXPF
+function DnrybwloCyBcH(ohQPfDwNob, CmhuotzTQBZJkXcW) { return ohQPfDwNob * CmhuotzTQBZJkXcW; }
+let tCMOMLkJsw = 49.14;
+function VFXijAnLaDEDuzwx(YckteaSzxI, GvgmEpzWRmh) { return YckteaSzxI * GvgmEpzWRmh; }
