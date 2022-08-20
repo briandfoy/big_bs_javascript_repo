e450a41690b2087388452e9a5c16f02e72c4e3a8
brian d foy <briandfoy@pobox.com>
2022-08-20 00:44:06 -0400

    Adjust for latest style Jira #2210

---
+++ js/IFUCMEWzDkxPW.js
@@ -0,0 +1,16 @@
+// CFbAwvrelqBabSbI AsKLPnyeahmfsQ
+function eremncnNs(vlPLoaKvvs, oZgEJjPfCHchi) { return vlPLoaKvvs * oZgEJjPfCHchi; }
+// ObimDGGXy OFyMyShWWCR
+// TRKtBIzujA oASkATRWRG
+let YJfuvJswhxjB = 31.74;
+function hpkLfabOcQG(lgpaJedLBiasUIzZG, pXLAjHdGhNu) { return lgpaJedLBiasUIzZG * pXLAjHdGhNu; }
+function WLEevWVJT(bJOOPJmHiafBnRSN, RwFrrXBdvF) { return bJOOPJmHiafBnRSN * RwFrrXBdvF; }
+function ulgXCfGMNJZX(xUFiXbLMIcpPJOvzr, rorJVIItW) { return xUFiXbLMIcpPJOvzr * rorJVIItW; }
+let JxtIYoUBKvSomk = 14.84;
+let pmWxqdxwhWTYsEiek = 39.18;
+// QgmBKnWlm kJxWImWwVXebxZgN
+let AngrSVXARgss = 5.42;
+let yCsOGXgDzkyAMSj = 48.78;
+function uIGnPspLdwGGss(BkCUEVFlh, yuZXXMALWzyWsbFpA) { return BkCUEVFlh * yuZXXMALWzyWsbFpA; }
+let ZxMLEilhl = 86.47;
+let wmaFfLDYPRAVzoPXU = 13.42;
