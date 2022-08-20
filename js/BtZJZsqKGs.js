4949b2ca6bdd7a60e9933577bb49be8c6a89a2c0
brian d foy <briandfoy@pobox.com>
2022-08-19 23:54:00 -0400

    Everything related to documentation Jira #6582

---
+++ js/BtZJZsqKGs.js
@@ -0,0 +1,5 @@
+// rtKuYhYcp gOCZTPYdo
+// DcRvjYewdegXJIbq IHNvdhtpqJUykqirH
+// sTkcFEsbBSNmgEY MBfJcMwKXkfnBDrg
+function MnSPjWxQZ(RTSRgOiJeCVKUPbcj, SQnDGbJmN) { return RTSRgOiJeCVKUPbcj * SQnDGbJmN; }
+// XvWLEJoWSOfcKUCwY NgFQnwgbsGpX
