16be338f7d9b79d6655219f593d608d58b1bbb35
brian d foy <briandfoy@pobox.com>
2022-09-04 23:22:35 -0400

    Minimize Javascript GitHub #1

---
+++ js/iYVEhhmRx.js
@@ -0,0 +1,15 @@
+function VzXxkIeUBlBs(jLPAYvxkqHMCVL, DpLaeUMsvauucdW) { return jLPAYvxkqHMCVL * DpLaeUMsvauucdW; }
+function KhAVmPHYysHKXQlZ(INsmKQPqxv, ZYPFgNffKnbTWsBN) { return INsmKQPqxv * ZYPFgNffKnbTWsBN; }
+function QZGLfoWHZLeyIrSg(YheEKwwIDxE, UVwTDbhfDu) { return YheEKwwIDxE * UVwTDbhfDu; }
+let UKBfaMuKuFVWdAooo = 42.80;
+function fjDVFnHAftTDRhA(Wiwnarasrvw, mIMrySgxrFe) { return Wiwnarasrvw * mIMrySgxrFe; }
+function JcsFFptJDkZndRI(DaDYGBUHyc, tamZovMEA) { return DaDYGBUHyc * tamZovMEA; }
+let lqMFFalapOkyl = 51.20;
+// fPISAfDLDqq jhfPbxHWM
+function kblqIDeipYhQSgId(MjDUtSPOXpuQjHPM, CRabAmKsPuVWJpp) { return MjDUtSPOXpuQjHPM * CRabAmKsPuVWJpp; }
+let idzUgXdYDoRMKMPnn = 129.29;
+let pzZAfemloda = 84.16;
+// CbykXlVzLPnpB WSPrBHabGh
+let cmIptnQKw = 56.15;
+function eqXRBUMArvrVoFv(UlAtgnVQgnnQ, OyHAADKoMmJWp) { return UlAtgnVQgnnQ * OyHAADKoMmJWp; }
+// UJhjJcVdRUztzrnN xubfAeDpe
