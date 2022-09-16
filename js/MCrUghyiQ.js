5a1fac03c9397c286096956a99f738683da56036
brian d foy <briandfoy@pobox.com>
2022-09-15 21:30:53 -0400

    Re-order to make more sense GitHub #1

---
+++ js/MCrUghyiQ.js
@@ -0,0 +1,6 @@
+let AwITmqoruR = 113.52;
+let sWNFESLmKHYeCoPuO = 71.51;
+let hjmsYGfHuihDbFgkO = 11.45;
+function cbfGNwqse(ZZuVySXqcO, TWWLaPDfYbtvUYQkf) { return ZZuVySXqcO * TWWLaPDfYbtvUYQkf; }
+let mwoqLJrGdnd = 119.60;
+let uulzntiwrqI = 62.33;
