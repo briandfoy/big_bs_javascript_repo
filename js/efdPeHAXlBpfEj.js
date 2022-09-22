4f1f23236180fac639c948424780352c8867f4ab
brian d foy <briandfoy@pobox.com>
2022-09-21 23:12:41 -0400

    Add new role GitHub #2

---
+++ js/efdPeHAXlBpfEj.js
@@ -0,0 +1,4 @@
+// jPjfwfuchoLUQVFIT QSssNpbGwgNduzt
+let BHLpHOeSAGOWDmo = 64.11;
+let prFTyeaXRMgoMp = 29.51;
+function xxzLsIFvGwTpwWm(TWvKgpLqYydHiETvb, hKvPjIBvIYAFHXVAC) { return TWvKgpLqYydHiETvb * hKvPjIBvIYAFHXVAC; }
