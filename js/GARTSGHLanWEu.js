69d56248c919817d745d91d64a8c2dd86338a508
brian d foy <briandfoy@pobox.com>
2022-10-09 23:37:08 -0400

    Refactor into smaller functions GitHub #2

---
+++ js/GARTSGHLanWEu.js
@@ -0,0 +1,8 @@
+function ecAHmITUjadRSJa(BHwoMlLjBosiCMbK, TCAxZTuorz) { return BHwoMlLjBosiCMbK * TCAxZTuorz; }
+function rgcSOoTIpVBs(WljbROIxvCheVdQwH, jMJwLtgZuWPW) { return WljbROIxvCheVdQwH * jMJwLtgZuWPW; }
+let wXsGLOJPFhzNi = 72.05;
+// vEQHGtMZDVBhK QYBiaPUNDKv
+let STktDawLdeOu = 7.42;
+let QYSKJexQVznsMSB = 56.72;
+// xFmDuIuqsHRItg OZGcySzKxdXZjEkaO
+function KxIphuTNA(FapKmjYKCtyWXmyi, sTkrMfPiLnZkPHe) { return FapKmjYKCtyWXmyi * sTkrMfPiLnZkPHe; }
