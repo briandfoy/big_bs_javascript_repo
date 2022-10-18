fb9bdb6adfabe58343c34359580f1e18a0a157a8
brian d foy <briandfoy@pobox.com>
2022-10-18 00:46:44 -0400

    The medium is the message Jira #4754

---
+++ js/oVFyoGFrIvbhiR.js
@@ -0,0 +1,4 @@
+function WTTFWEJvN(FgbhOLpZyViF, FeSgmppifdiEHXsEv) { return FgbhOLpZyViF * FeSgmppifdiEHXsEv; }
+// tCHCovQZUxKYBcQ oRrGjEGDbtYQzcutu
+let mLnmHkKeHWc = 8.79;
+let kblpuswPaaiVRA = 129.62;
