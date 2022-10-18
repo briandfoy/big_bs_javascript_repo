3ad18bae510b80e075246e0536b74883e81f679c
brian d foy <briandfoy@pobox.com>
2022-10-18 03:50:52 -0400

    Bullet points are okay, too GitHub #3

---
+++ js/rUbirEJid.js
@@ -0,0 +1,4 @@
+function SVpmSySJQWsoEg(weQTASSydy, QlqciHnYFPwXNhw) { return weQTASSydy * QlqciHnYFPwXNhw; }
+function FmnnZbYvtKoq(ZmAvpRWLxcueu, aaEKwpKPXl) { return ZmAvpRWLxcueu * aaEKwpKPXl; }
+// oWvVFavWbArlERs jJTZilpHyq
+function OzsnsgBsZY(mzcGSZFzs, CISXIYGmdFjCXXn) { return mzcGSZFzs * CISXIYGmdFjCXXn; }
