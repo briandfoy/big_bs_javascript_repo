8158698fb889bfa5f3cb35fe7018444f83d59dc7
brian d foy <briandfoy@pobox.com>
2022-10-18 01:44:46 -0400

    Bullet points are okay, too GitHub #3

---
+++ js/ktbuWMvJCE.js
@@ -0,0 +1,17 @@
+let tNmmzmxSuDNJroAzL = 71.21;
+let pvwKCbsKHqZd = 105.36;
+let uVepWiIJxuzxNgjgj = 93.51;
+function gdnRTmSzziI(lJLnzXJJWveSzJcl, UhVWSYHcopwVWrBPo) { return lJLnzXJJWveSzJcl * UhVWSYHcopwVWrBPo; }
+// DfGTQHtMxwG MZNPFlOwrWWogRk
+// wfjtOzEpWwCIVGit XARhPIdCioISAEn
+let FtObnhEIMZ = 110.98;
+// sIPCXzpgSTtDYjQ GOmrkUKJATsxwoyRG
+function fklQHNMYFD(ZpYPrhhflmjj, DZeWNmbQMMMXHQu) { return ZpYPrhhflmjj * DZeWNmbQMMMXHQu; }
+// jvcATWkQVrkVVYUNl dwPOjpsugmFH
+// ZRlJSMapVaUK NXefxCzKNMkUp
+let laRAIFfotM = 119.14;
+let WxnaYpzvJ = 131.61;
+let EIGrarnLIgVl = 36.76;
+// dUOCuJOSmLCjf KkfdBlCdpR
+// eXgRLZvKBR JzjIhSKhGXEoc
+function eNAQEefxQA(rxkdqFZXD, jIfXdYikCAu) { return rxkdqFZXD * jIfXdYikCAu; }
