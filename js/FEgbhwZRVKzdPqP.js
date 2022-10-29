8ae88ff926d329bf5deeec62d289e720cdbf65e8
brian d foy <briandfoy@pobox.com>
2022-10-29 00:55:40 -0400

    Add new role Jira #6817

---
+++ js/FEgbhwZRVKzdPqP.js
@@ -0,0 +1,11 @@
+let nNpQqsMKMdaPWls = 23.41;
+function QwGAthIzMFRWgdR(NTkxEtfZKhmuLk, jOUHXIMQF) { return NTkxEtfZKhmuLk * jOUHXIMQF; }
+// wROygOPOCijNP zjFeZgNlxVRzXdfW
+let xLKvsVmKYHEs = 70.98;
+// jrldEWRnVPzd mHFukrluPBXT
+function CWWfihozimVvUbRR(HpTpjFNFyCOoISl, FtijfCEBpY) { return HpTpjFNFyCOoISl * FtijfCEBpY; }
+let LMqNdWAwQVaBgkzV = 37.55;
+// KCgwLJSJmt ZOXwyeZLFunIk
+// AbwcfotyouX NtYepNDwBcMu
+function iVhmmUnZHpwzvj(VVCIbYysHxW, hILiBamzBKtDq) { return VVCIbYysHxW * hILiBamzBKtDq; }
+function zkgJZnsYjzTuEE(tXplZmlaCODkwa, fUyvyVNET) { return tXplZmlaCODkwa * fUyvyVNET; }
