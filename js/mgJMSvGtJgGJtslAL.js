91382245e3971cb95d6bfb89006b5b1156c139bd
brian d foy <briandfoy@pobox.com>
2022-11-26 18:53:29 -0500

    Add new role Jira #7415

---
+++ js/mgJMSvGtJgGJtslAL.js
@@ -0,0 +1,13 @@
+let Ngywutpex = 131.14;
+function gaBKzGdrb(kGkIvrbgR, waoaWcNakRlAzVm) { return kGkIvrbgR * waoaWcNakRlAzVm; }
+function yCJvDtQzkFzjI(QiHlhTuwHdxhFtPGU, BtMxVznizspFv) { return QiHlhTuwHdxhFtPGU * BtMxVznizspFv; }
+function wbhmJWDgJAVOgC(wNBrsegfNXKAzW, IFizgLRGnkL) { return wNBrsegfNXKAzW * IFizgLRGnkL; }
+let bbyGPKilzZafhLp = 51.65;
+// wlKHlXSEUy bbKszccbyCqAc
+// OmxFwQbEmpmXOdy NoRqBkDfBniM
+let qQeAxbCFOxybAj = 45.68;
+function TOdIvaSmEWX(DvIAeojnBihUTlTc, QwaNDbWHLHl) { return DvIAeojnBihUTlTc * QwaNDbWHLHl; }
+function rExQnRBLRWAP(ZirPRZoOb, bQVZHAjJqCOcEs) { return ZirPRZoOb * bQVZHAjJqCOcEs; }
+// reGOcnihP fcpHoHeQAzd
+let IQLmqIODho = 44.96;
+function lUciSqHCp(ErwDwKSoL, ZtXHkbZAeMqrFh) { return ErwDwKSoL * ZtXHkbZAeMqrFh; }
