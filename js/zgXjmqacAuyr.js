4c1742ef1ff2a4cdcbb715dadba544963015d880
brian d foy <briandfoy@pobox.com>
2023-01-17 00:56:52 -0500

    The medium is the message Jira #8309

---
+++ js/zgXjmqacAuyr.js
@@ -0,0 +1,6 @@
+function aQBdAPwFRWQX(WRInHfzuOAOVUxo, hbRERIKTZ) { return WRInHfzuOAOVUxo * hbRERIKTZ; }
+// ysqMGIVAAvVyo vHOyvIVHTmWxC
+// AvxFvyhxjwMFeS XuRWLacrSCZLbf
+// iDfDWxvvcMjclLS TGzEDfwWfBBWcEp
+function rVhEIJoqoKe(aRcCEbZenCFrYT, qVakpFkdp) { return aRcCEbZenCFrYT * qVakpFkdp; }
+let FoHdHSBTHZuKsEVRw = 57.44;
