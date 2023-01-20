5d59790cd36c23a9165603c62c34847015e2f43c
brian d foy <briandfoy@pobox.com>
2023-01-20 18:22:41 -0500

    The medium is the message Jira #2

---
+++ js/tWIXHleHysf.js
@@ -0,0 +1,9 @@
+function JCbEWSssQPbUoE(FdaRKTlbA, WHsvLXSGFaY) { return FdaRKTlbA * WHsvLXSGFaY; }
+let fCGYrHgNBvWPzWC = 1.19;
+let NLyAxduicSxnmJOEX = 71.39;
+let gdvSZLyedGj = 6.27;
+let EDKHAgfbPZb = 6.00;
+let VKSgBLCvxkAxJLuo = 3.59;
+// BSqkgNQMwSdX jpdXCSfyKXo
+let tLCybbAzT = 107.59;
+let lqnOEMpNOQry = 15.96;
