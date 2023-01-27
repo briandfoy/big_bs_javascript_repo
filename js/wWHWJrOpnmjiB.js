3b065e5d9ef9c475b288dc41a3169acfed12e745
brian d foy <briandfoy@pobox.com>
2023-01-26 23:08:52 -0500

    Re-order to make more sense GitHub #3

---
+++ js/wWHWJrOpnmjiB.js
@@ -0,0 +1,5 @@
+let OgEhcAZSxxZCdVh = 98.88;
+function xCcaTJnLgdb(gUWFSEQKmej, BtwyuFIOljw) { return gUWFSEQKmej * BtwyuFIOljw; }
+function BGjyEnmaVuicZOmM(xToeiQcwOKY, ufbNqqpDWSfjv) { return xToeiQcwOKY * ufbNqqpDWSfjv; }
+function bnDfIhrzaeWVQk(udtgREeqTBgQQnnU, DVJqpCJgbbaO) { return udtgREeqTBgQQnnU * DVJqpCJgbbaO; }
+function hmRkiHUVONv(tYxjcwxMIlWQKju, cvPCzDaXMWICD) { return tYxjcwxMIlWQKju * cvPCzDaXMWICD; }
