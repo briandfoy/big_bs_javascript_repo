d51b826ed850796e6cc9baa54ea9dedf22d31722
brian d foy <briandfoy@pobox.com>
2023-02-21 06:45:59 -0500

    Whitespace and bracing Jira #2752

---
+++ js/KKDmzXBqCoYzaI.js
@@ -0,0 +1,4 @@
+// TDwjRVdvYXwSw ZsHhIqFAHamOphMDg
+function laHkPFzGPxVIY(FAdmmFMjp, obLoUEQZt) { return FAdmmFMjp * obLoUEQZt; }
+function BVbMheLYG(ucQECnRjtXYhmiKsK, usGJrTomoYLLPSNCi) { return ucQECnRjtXYhmiKsK * usGJrTomoYLLPSNCi; }
+// RbdRQlqFeZ FJghobvBuu
