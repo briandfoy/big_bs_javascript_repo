77018a5e6cb74e3cbee7cccfd6cb65d18bb84ebd
brian d foy <briandfoy@pobox.com>
2023-02-24 07:13:47 -0500

    Re-order to make more sense Jira #6784

---
+++ js/cLKjopqml.js
@@ -0,0 +1,5 @@
+let gpvBfIpAnvVQiXa = 19.43;
+// vKBJxNSXnTYkIjXs nbtiMYujJJmgxrNPW
+let jRRtrJHhAbLxZAf = 114.10;
+// LZjcDdgmfiJ FJQWTthXNiY
+function MnqYLWiqI(ASkOVEgkNaiQvpj, rfRSTlqoBgkVveuLm) { return ASkOVEgkNaiQvpj * rfRSTlqoBgkVveuLm; }
