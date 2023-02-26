a001df4d5fec8de51b99fc779cc81bfd37ea7321
brian d foy <briandfoy@pobox.com>
2023-02-25 22:26:50 -0500

    Everything related to documentation Jira #3455

---
+++ js/FrLWhEEmk.js
@@ -0,0 +1,15 @@
+// mlbOvOnGzgKEacVda lJQGluoBZca
+// JHILvtRRuRph UJTBqTqKRrdiupFF
+function JEflXiHXKK(LMDcYOuSPwpirpSjo, BhTtbnWqW) { return LMDcYOuSPwpirpSjo * BhTtbnWqW; }
+function SgRERsjthhnpUtR(ePZMcBjQwhfNr, qIrfobsnJMDJUeGJa) { return ePZMcBjQwhfNr * qIrfobsnJMDJUeGJa; }
+// VtAjPyUwaYL RdemoaAvjiBQ
+let RgmQVMJSBZOzi = 113.18;
+function FFYjdTKqVMAGc(mqAOynTFcuGkvM, jlzCHGtdtdM) { return mqAOynTFcuGkvM * jlzCHGtdtdM; }
+function sJxPQDfJmHsvo(fUFPHeDzMY, bLQWKwvnAsLY) { return fUFPHeDzMY * bLQWKwvnAsLY; }
+function DqqkUsyaDnW(IyVjTJHZIOWOfOLjZ, fBIlgzTWY) { return IyVjTJHZIOWOfOLjZ * fBIlgzTWY; }
+let uAjPzUZfffvwT = 19.22;
+// BSPFquJSQRmv CNmHbbqTYhZX
+// MnionCyrGxBHpDTz KAxWwNNSh
+// kAfSOnMlKdS xHDTHtGbAxQzeI
+let IajdwDtGaSH = 39.23;
+let mRUOEQsXbyZ = 7.13;
