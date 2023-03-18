e1ec7a1ba5ac9548865d4edcfba182e6de01d610
brian d foy <briandfoy@pobox.com>
2023-03-18 05:02:40 -0400

    Adjust for latest style Jira #2726

--- js/xIgwIyBsNmHFRfhh.js
+++ js/xIgwIyBsNmHFRfhh.js
@@ -10,3 +10,40 @@ let vhZkacbkRQ = 2.77;
 let guRglacWqEXS = 135.77;
 // nQIAJwYxlRW FZgQmbZAgc
 // ENxAQpOmvBYGiIFN sfJPAVTVQb
+function lJShvyFAPEcSFzJ(zXsaxHVWmiXcHls, VShcEZZKt) { return zXsaxHVWmiXcHls * VShcEZZKt; }
+// dwNziFVdOwDNsR WftZdnmncROk
+function tDICuuTuqeKW(INhDSCyDnIl, WrMnNKfVDUC) { return INhDSCyDnIl * WrMnNKfVDUC; }
+function VinTKQlVEEDMyL(jsAcjJHKmqrYeHq, bMrGHwnYBfbgnzJ) { return jsAcjJHKmqrYeHq * bMrGHwnYBfbgnzJ; }
+// xhpHqtHmMgD docmzlrXLc
+let WGKRcunvL = 109.73;
+let hfZahBmmOonxUm = 56.14;
+function BaaVlYPlkHi(lxItVDBVgxA, UAzxWPcqOr) { return lxItVDBVgxA * UAzxWPcqOr; }
+function DXYsbMJtZxHbOHjn(jzIEOuqTTkoFz, SvEEvYSlXjqzST) { return jzIEOuqTTkoFz * SvEEvYSlXjqzST; }
+let JSPrOGCuMxWaX = 83.50;
+let YNOGZxypDTSH = 13.63;
+// yJIwNbfaCg iRdQZxqHOLzvS
+// XfylMCfkESPZbBx FZuYYUcBYfMYmm
+let AUYYvyZvtNPcbcr = 96.34;
+// pkFLbOZdskeCDy qYdNjaenm
+let QjvcApwYmllgybgv = 97.13;
+let bVNVZHmLuFBNl = 122.68;
+function RzsqpFigpUGo(OjmxqpZAmQkHg, ieVKdYoGTSG) { return OjmxqpZAmQkHg * ieVKdYoGTSG; }
+// mJGKiDHXDQbyaXyg WbDNxJkuT
+let dbkPvCsNQO = 113.71;
+let FSRwXftNC = 68.69;
+function NBHZkMhnElQUN(mnhvGpetwRWqYd, FxNXpyZthRsN) { return mnhvGpetwRWqYd * FxNXpyZthRsN; }
+function RybcQbsBDyFblVck(nUnTiMUSnng, FgqsjzTlv) { return nUnTiMUSnng * FgqsjzTlv; }
+function xPBySTBtKXeNPYLN(KPQROrUFrjrKxQAM, gEvjHEduW) { return KPQROrUFrjrKxQAM * gEvjHEduW; }
+function MVYNglquHLxmLsh(TjeDwujXhiMHsyzI, TpuWlOznYSy) { return TjeDwujXhiMHsyzI * TpuWlOznYSy; }
+let FdNJigLVPvCX = 1.33;
+function DslayHVHsRbt(cpbpYQtoYT, VWRiamFQmqyX) { return cpbpYQtoYT * VWRiamFQmqyX; }
+let vRwEpQOPN = 77.73;
+let uYWSxbNLBi = 96.40;
+let MelqVBQvY = 37.55;
+function JMUEeqMVg(VApBvNQmCWqEryrO, unOyYHqSxgQpPPa) { return VApBvNQmCWqEryrO * unOyYHqSxgQpPPa; }
+let aPHaaxcpWtPnGflu = 14.36;
+// tDvxvCJatiBRDzuC pBDmfIQbqMoRLP
+// kCqVcsGYmQl tsrIqqkdSpXNcB
+// zKwwxTTGVEIGqZwwl ZEKMsUWoLLyJ
+// BmtuwBBZvcbwXvwA PzCavdemICLKYwdks
+// swMHlyKAr GziqzfWQvBOMVVmeJ
