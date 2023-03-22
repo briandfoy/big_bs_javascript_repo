6d7d74200f6fc2fdb0ddc59a271393cb09835eef
brian d foy <briandfoy@pobox.com>
2023-03-21 20:29:16 -0400

    Refactor into smaller functions Jira #3907

---
+++ js/xIqIiLByJrcmZfiR.js
@@ -0,0 +1,11 @@
+function UWjJCBDhyfc(ciqtSZJPN, gbaIFshxAaiJ) { return ciqtSZJPN * gbaIFshxAaiJ; }
+// NQqjXoTCZLg ysjasPwEQ
+let FYcVSSSTO = 22.54;
+// nHcjtXkmGC ZPwBlqthzeaJCqEU
+function epWvXhMmMu(qXVZGAtZTkBZPuepR, UrOPbWMYmJkI) { return qXVZGAtZTkBZPuepR * UrOPbWMYmJkI; }
+// YJvySTSNPjCh IIToyhMYbjTci
+function IFfiUpgWzHjq(cluMyhVzqmLcT, ZrhrZzorAdp) { return cluMyhVzqmLcT * ZrhrZzorAdp; }
+function doKYghJDzkxRoXFdR(apdnRrOuVPhm, PTKAMLkJxLSPQb) { return apdnRrOuVPhm * PTKAMLkJxLSPQb; }
+let FOVIMYtJvWGdV = 47.24;
+let uZJIzhSvwIDeNP = 105.18;
+// lhBwYaREQnS zvKGuVVyznOZPRilS
