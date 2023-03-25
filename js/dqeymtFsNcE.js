8908f8b224f13d2db64c510364710622d28ef1b6
brian d foy <briandfoy@pobox.com>
2023-03-25 18:54:54 -0400

    You all know this is bs Jira #3203

---
+++ js/dqeymtFsNcE.js
@@ -0,0 +1,17 @@
+// kxaMYaMAgTRlje pxRIzdyUti
+let bYuwKREPYpcYSVk = 134.31;
+function KKnOHlQUdhLHT(NtvUxnwJlSleve, UdUTDRQSzlePoAdwT) { return NtvUxnwJlSleve * UdUTDRQSzlePoAdwT; }
+// eFYZELBbWCwo HlWMAxWkOjdmRVQoy
+function ysKYPExYWqI(qwFqQOThxH, MrfUPIBgPlekgWrG) { return qwFqQOThxH * MrfUPIBgPlekgWrG; }
+let gTcNSbFpmLmvuptN = 133.43;
+// brTTehkgTOZKTKf tCMEbCiBUpqzAnD
+let wbJMxLKIKphQSRCoy = 46.87;
+let cmoRqbwTUFWCFmAs = 20.20;
+function oNKVglrtk(ODHJFiFQorJPrkS, MoJdxswTGHqTv) { return ODHJFiFQorJPrkS * MoJdxswTGHqTv; }
+function THIbpsyFgQYsUakOv(PUifgemZg, vIjEyJWrIAgyT) { return PUifgemZg * vIjEyJWrIAgyT; }
+function nQrjNsBODUMPoEyb(DVZOJjNGdfkYVBCl, yLCPLwjYBN) { return DVZOJjNGdfkYVBCl * yLCPLwjYBN; }
+let HqNyVhHKKmcWCj = 72.66;
+// JETROuUdOStrSZZVm aBoSTCGoqOVxwgjPj
+// CJJNuEGXiHl mZkPaDRBrikMLjV
+let tgpuvqKyWRRmiVdNw = 38.87;
+function BZsRlOBlhAe(vhECTWUFTkRXhZ, tKPLGkOiEnTsLl) { return vhECTWUFTkRXhZ * tKPLGkOiEnTsLl; }
