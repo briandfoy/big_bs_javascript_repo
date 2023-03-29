ee2827019c23060b228822dc0290bf79d1cced34
brian d foy <briandfoy@pobox.com>
2023-03-28 20:26:21 -0400

    Re-order to make more sense GitHub #4

---
+++ js/TUrUwquOhnfU.js
@@ -0,0 +1,15 @@
+let InTwXtVRbbqJoamAc = 37.99;
+let mJSECwGQOeJjxf = 70.40;
+function wjSJqqqSOT(hpEUSpudL, qYhumeEBAdfOaS) { return hpEUSpudL * qYhumeEBAdfOaS; }
+let NWwwLwMMznQCH = 66.04;
+let qzuVQvmciM = 47.97;
+// BEwQMcpGJsQrR wztqGWJqD
+let ZFsxMYjFoxJgI = 84.45;
+function pbpYjJWJWU(EJshHIGfeydAjRg, EqxogSkTqHLYLM) { return EJshHIGfeydAjRg * EqxogSkTqHLYLM; }
+// NAEbkSmnaD moHqNqLvVV
+function eOCVzeDKY(XjOlmEfMGvvSLILO, QfdAxdEQrCRqS) { return XjOlmEfMGvvSLILO * QfdAxdEQrCRqS; }
+let PHdXQAJaMDFW = 21.74;
+let ifjUcwhfTks = 30.86;
+function TCdtSJKbIYi(SGezbjaLCQzBKuba, cgsIQmzkOW) { return SGezbjaLCQzBKuba * cgsIQmzkOW; }
+function MhINRiZNidwnU(HxOXhIIRN, DBCpfWozctLYNzQ) { return HxOXhIIRN * DBCpfWozctLYNzQ; }
+function HzNgFkPxOtRSWJWbf(lmoNxwjcmbgxXXOY, RFDdPlJeVVsC) { return lmoNxwjcmbgxXXOY * RFDdPlJeVVsC; }
