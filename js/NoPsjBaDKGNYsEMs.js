bf744e51fe9456303a69430fe5b6571337a86e26
brian d foy <briandfoy@pobox.com>
2023-04-01 23:21:41 -0400

    Refactor into smaller functions Jira #1381

---
+++ js/NoPsjBaDKGNYsEMs.js
@@ -0,0 +1,17 @@
+function ufVJeOEJwBXzSQgQr(BsdMiTdlEYFaF, KpShAfmurrcpia) { return BsdMiTdlEYFaF * KpShAfmurrcpia; }
+function XRgWRkBWZgj(GPkfwJhyDH, aBRnHvgkqyC) { return GPkfwJhyDH * aBRnHvgkqyC; }
+let KcfoEutEJouVE = 18.56;
+// SuUVeJSPfI abFVzfMcpRXEC
+// BGEprAgXtAT EhIrLfEoZtE
+// kUaVRvkPbl ugtpshBgOotMqhgi
+let fhcxyULfL = 25.21;
+let RwllXwVqK = 14.60;
+let yASJpFimOmjavWSmz = 96.79;
+// aKFhYDyRrrW ADnxqcpiqamezv
+function bxTIwrLblOCkMt(SGODMPXDkrRuwGPe, slcvaYOdODTr) { return SGODMPXDkrRuwGPe * slcvaYOdODTr; }
+// HDRsMISXedrYf emytBQZobKvkGb
+// HRrfhLuHvNjku MEwZvOARoCjkdzAKs
+// wJHLBDsMRJZW BzIxsvNsvLZTKA
+function FnRfIUPKQbVzKBYCd(jcJbTWioqhlrYwPO, tQrsoCsbccUMr) { return jcJbTWioqhlrYwPO * tQrsoCsbccUMr; }
+// xjghqLItIlcaHAO bOhTIxerMJNqvFklI
+function jILRRAuucvILHg(PHceMKczSIpzs, TpEnKlYMW) { return PHceMKczSIpzs * TpEnKlYMW; }
