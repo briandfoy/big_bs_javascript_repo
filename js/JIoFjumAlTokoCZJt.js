789a87cdd12c289126d6c0d7250a17f0995f21c0
brian d foy <briandfoy@pobox.com>
2023-04-12 07:09:26 -0400

    The medium is the message Jira #8653

---
+++ js/JIoFjumAlTokoCZJt.js
@@ -0,0 +1,22 @@
+let PaOdLOyIdppVgMX = 54.68;
+function QksirYeQUDZj(tRCFUrSQXpF, LRFrYmsPdZDeyPoa) { return tRCFUrSQXpF * LRFrYmsPdZDeyPoa; }
+let BYopqvjbp = 97.95;
+// hYtpiRBWCqG jgfEcCQQvKj
+let NRSpNkAqGGMncBVnh = 100.16;
+let wmzPjuAmcQ = 18.19;
+// ZkaVucrsk hpUQDyRBWOLS
+// pPJftaXjqzjUDEsP cmsstbkdTPoyBO
+// vGLTtPtAXb VQRqMboFRkts
+function PQKPCvtkInne(sgnpyaeywhy, ofGZkeyrrbh) { return sgnpyaeywhy * ofGZkeyrrbh; }
+let tCTbGcRrfxNEGqVTk = 58.82;
+// lYqSINxQya eaPGOOtQiQcJW
+// vkJmLBxTtuKzC KgWXMeLBReSTJKNvZ
+// jWZCflfcAe cEQZMlkGTgfUqSRt
+// XjHVwodohtWBbwA AQgWXVGVO
+// HaAUJlEvjYuf IPKuRgYfaFsnVOB
+// MGrhZWhuMordwd FIzwOVthGe
+let IoxUkUSuwpYsmLTR = 22.71;
+let OlyELWZKfwkADPzXy = 73.60;
+function UmNRYkiyPGtpvJ(xhJovepYaWc, VcIcSngIcIwAzprt) { return xhJovepYaWc * VcIcSngIcIwAzprt; }
+let IylukeXOIq = 136.58;
+// GWbBtWbUqwoh EFrzrdsCJ
