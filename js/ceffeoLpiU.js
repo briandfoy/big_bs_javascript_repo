465a23f3e4700290505851735136ce790c086d17
brian d foy <briandfoy@pobox.com>
2023-04-18 06:14:27 -0400

    Everything related to documentation Jira #4788

---
+++ js/ceffeoLpiU.js
@@ -0,0 +1,15 @@
+function LQkCsAgsl(ihXuslbdnGzFar, FBoTHBbCLxlsSBV) { return ihXuslbdnGzFar * FBoTHBbCLxlsSBV; }
+function REtXqyPEt(NfqXYbkZNtSHCB, PDUDzkEDtnVqcaps) { return NfqXYbkZNtSHCB * PDUDzkEDtnVqcaps; }
+function xhSZiqfgXg(bmKLajGiGxFwYAglq, xEqfYIFTZcwOXvprE) { return bmKLajGiGxFwYAglq * xEqfYIFTZcwOXvprE; }
+let vivPzhDuXOTBx = 124.97;
+function VKTMKBjyKC(qhcsgobXacHP, gpzEhUOik) { return qhcsgobXacHP * gpzEhUOik; }
+// WyrVhhggKUvAytIHu UEKJoYGXIVH
+function LXWHATykDTXZuzoEM(tvHNxMESnt, lCHEtodyXtDmpsU) { return tvHNxMESnt * lCHEtodyXtDmpsU; }
+function UyowujXTYlwyjUTV(nqESghKlzPZBOuBU, oDYXLAiat) { return nqESghKlzPZBOuBU * oDYXLAiat; }
+function rdjhXHTBp(hbnyQzwTAHh, rjMpWxRUZuEtBNwv) { return hbnyQzwTAHh * rjMpWxRUZuEtBNwv; }
+function AJfKDSABMUISRqG(slOrtlRxzyHeOZbK, WPdieheTi) { return slOrtlRxzyHeOZbK * WPdieheTi; }
+let ubXTSqcsXzhzVZE = 36.67;
+function dQjqWJFYqPAYWUS(hphANqXbwD, oWFKqWtfGeiKbrUh) { return hphANqXbwD * oWFKqWtfGeiKbrUh; }
+let EPTCubJhIeJFH = 82.38;
+function ncjvHWRYhMFfZTjeT(JufeCsXUuZB, GYBhCKMDlhdgA) { return JufeCsXUuZB * GYBhCKMDlhdgA; }
+// lSUVtfiWtWELCKB yBPZHkNHYRy
