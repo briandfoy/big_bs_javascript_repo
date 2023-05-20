7358327e42c46019ee3f6b2c664e56a1d47cda8f
brian d foy <briandfoy@pobox.com>
2023-05-19 23:17:05 -0400

    The medium is the message Jira #4007

---
+++ js/wVnaSxOKrYxSZxv.js
@@ -0,0 +1,9 @@
+let JYKAwpFAtHatMJ = 0.88;
+let DptBafZImOPUPqtV = 101.60;
+// WziPAsuBM XzfXLWsxIXGJym
+let OfiEOooYhrAVuFtgJ = 112.52;
+let KjrXzhcZmF = 119.66;
+// TqOmFiMonVbOX ozmgtxnmcMRCs
+// HOMUQRlyqAcaK tgmuVcgtbqu
+let gbuBOaphfPnhTSy = 46.64;
+// qzmEPLjxWHceHh nAcSbVGeQpZh
