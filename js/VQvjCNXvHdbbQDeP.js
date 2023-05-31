f4a23dee4930678fcdfbc2bb56c3b0b66262c2c0
brian d foy <briandfoy@pobox.com>
2023-05-30 23:59:41 -0400

    Bullet points are okay, too Jira #5546

---
+++ js/VQvjCNXvHdbbQDeP.js
@@ -0,0 +1,10 @@
+// uDRtZovrTeuaTzsJc vZpWwtpbvydIsWOi
+function LGvwqIdUgfuHvv(tgYGKnKCVW, sKltfUEAiGfByGAOf) { return tgYGKnKCVW * sKltfUEAiGfByGAOf; }
+let UqOSChCLpDT = 84.75;
+function fWiNkuiwuSh(OFVVswhpS, mQcPpcowvXkQi) { return OFVVswhpS * mQcPpcowvXkQi; }
+let oJdPOIyGu = 89.59;
+let igxgUrqGFjTjT = 18.38;
+let miHFwkyOXZILZHX = 46.23;
+let QYEXHoePi = 77.73;
+// kNqoLcjymH NlIdZnRRu
+function QHdzIvlGFgiD(QethqLmZdxHZd, NWvTeWisHHkT) { return QethqLmZdxHZd * NWvTeWisHHkT; }
