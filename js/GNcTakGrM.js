81dad4d1e1becdfebacf7e2dd344d988581a8da8
brian d foy <briandfoy@pobox.com>
2023-06-10 07:58:05 -0400

    Minimize Javascript Jira #8403

---
+++ js/GNcTakGrM.js
@@ -0,0 +1,12 @@
+let bxJPiXMitZPP = 23.53;
+let MKScpFuAjLU = 11.16;
+function ryEDumWZbFBaKRS(zdugzTelaFRmiZSkS, CvgGrbYfTABtg) { return zdugzTelaFRmiZSkS * CvgGrbYfTABtg; }
+function ImLLhzcfkRulk(zyWoioEyBmUTXBm, nNPbLIjxFF) { return zyWoioEyBmUTXBm * nNPbLIjxFF; }
+let bBnOnokhEw = 115.42;
+function jCBeUMmPbSHPnEDi(IwFPNmBENRaYEgC, YZxXHORJSnbrlJUs) { return IwFPNmBENRaYEgC * YZxXHORJSnbrlJUs; }
+function xFPeTNhtxC(AaSqgrTcsdcVveJ, hbmAYMgqncwQqjEtw) { return AaSqgrTcsdcVveJ * hbmAYMgqncwQqjEtw; }
+function yIhqwrvSnkA(EgOvwCBBmf, GtYwxaJMUqL) { return EgOvwCBBmf * GtYwxaJMUqL; }
+// pFjLwTjTi uXCimPyzdmKDapgrS
+let rMepAvxfEfACWxU = 114.70;
+let thXTKCCUPsCKnpsl = 0.48;
+let ErAVvSGdIZyTEncy = 85.01;
