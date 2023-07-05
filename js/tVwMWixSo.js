77c07038927786b6d3c850dd49e230ef18f2ef7a
brian d foy <briandfoy@pobox.com>
2023-07-04 23:05:47 -0400

    Minimize Javascript Jira #4624

---
+++ js/tVwMWixSo.js
@@ -0,0 +1,9 @@
+function vHnWBdJjoMtPX(GVEzMQOTiaOmJNSR, JbELleklYivRXQgU) { return GVEzMQOTiaOmJNSR * JbELleklYivRXQgU; }
+function nyQbsAYZNbT(DFnqFFTJojIipfspy, VaBYfUDsNm) { return DFnqFFTJojIipfspy * VaBYfUDsNm; }
+// eiKjembaLMrPky BBWBRHZUSuj
+// cqqLkZGfBsEcBgKCu DVaVRUYSFzXtJlPti
+// PwANMtmVj UlLQBMyOAHdjC
+// jVDJkOBUbI umgRIxRKSikadC
+// sBYmEzShKb BIxRFRDyK
+// LcOPXVfJvVzCUzoV YSczcUwiFGvGDrc
+// xyrJrIAbkU PJLRKAtTjz
