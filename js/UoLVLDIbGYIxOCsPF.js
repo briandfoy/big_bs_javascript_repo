c96c2c78c5dd3b7d36d43d2d710351bf1ce3793f
brian d foy <briandfoy@pobox.com>
2023-07-13 07:10:27 -0400

    Re-order to make more sense GitHub #4

---
+++ js/UoLVLDIbGYIxOCsPF.js
@@ -0,0 +1,6 @@
+let ZEGVGPbVLIZyo = 20.92;
+// cuNZrjTrgZeCyt rJgHTcgiczEID
+let IualFhkLMKoEJE = 77.16;
+function XwdRPUnTXHibmkO(CTpZEuSuAkYtKkDJF, yASRqoZoiwCJjdQXa) { return CTpZEuSuAkYtKkDJF * yASRqoZoiwCJjdQXa; }
+function hVGAnDtKTnX(rvVFJoRdzp, dZSKfBnFBL) { return rvVFJoRdzp * dZSKfBnFBL; }
+// oXYXOQSie aPkPfgvHCeBZCVC
