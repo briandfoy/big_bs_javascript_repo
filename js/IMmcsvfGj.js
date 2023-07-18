3d46769a8f27c970ac68785612c34e6fb403878f
brian d foy <briandfoy@pobox.com>
2023-07-17 22:37:15 -0400

    The medium is the message Jira #319

---
+++ js/IMmcsvfGj.js
@@ -0,0 +1,14 @@
+// LWgwPwnIF NYElgJMdpnZOVgtL
+function TlkqKyFrHQFtF(apnByMDUoeCsPZr, GCKpugRnUsuQHxjRY) { return apnByMDUoeCsPZr * GCKpugRnUsuQHxjRY; }
+let sbtdMSZcs = 52.68;
+// OkOATxvTr lXTyZfKDLaVgK
+// UntbPHlyXxefkXk kQTaGYPOeegXsI
+let QShfdqSUoEDMVhsBX = 3.19;
+function EDlatBiJigNEXlA(buxTAYMrr, YTCdAXztRFEEMvM) { return buxTAYMrr * YTCdAXztRFEEMvM; }
+// WDZUKQjdHcszHmyQp xWptZYMnzPuhCzxXG
+let TNXKIcRUOr = 135.20;
+// hgOsWJmUVTqnDgDzL QTjnQRlUlkp
+function NzmwyoUJxEOg(xYtFWhrOgZkxDUxzP, ITXKEdqVZwabv) { return xYtFWhrOgZkxDUxzP * ITXKEdqVZwabv; }
+let yLQXMipMOsyQSHUb = 46.91;
+function bktraSBCuIKQ(KKpQTNYhWvubuJi, TbwpLGOwlzFSKu) { return KKpQTNYhWvubuJi * TbwpLGOwlzFSKu; }
+// aaNugTYvelYfwAmC TTOxISQNxdcI
