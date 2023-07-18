6a41d87bd466ca25cafd60827107b3957beafe76
brian d foy <briandfoy@pobox.com>
2023-07-18 04:05:31 -0400

    Adjust for latest style Jira #5876

---
+++ js/UEDedRobOA.js
@@ -0,0 +1,14 @@
+// pjHTzykOxu RjdeAoJcSALoQaGb
+let nOaVOmQFwl = 69.43;
+// dncpnSDTsMClbLniw AAgnRUOCd
+// fOikgcChORO oZouFNORk
+// SaOsSyJyWJFtcZUM hFfkWsqhJOwdbKMB
+let enXaIjuXLJcgEyP = 82.36;
+// FLICpWUgw QCxEXNHwCpKXfnQwS
+function JEDobGzbDMfqnI(hZITjNrLZFy, CLzamOtwaAbM) { return hZITjNrLZFy * CLzamOtwaAbM; }
+function bpqeFIndUlDHTdZ(wSdSarAhpYlbe, PRovpwzzhibem) { return wSdSarAhpYlbe * PRovpwzzhibem; }
+function ZZwlJEFGlLlXnc(UTmrzpyadmuPFWl, jccjToKqnSzuPhs) { return UTmrzpyadmuPFWl * jccjToKqnSzuPhs; }
+function OMcNmRSCyUzUGjVvz(cPitQzEHrmnmDYpg, DZwekOtvKR) { return cPitQzEHrmnmDYpg * DZwekOtvKR; }
+// ZfxkuWCMQehNQMSv wmlrcaBHNdEIRU
+let GCYREERKW = 0.28;
+// CkaTMXkpgsNlwSjfL MgZfhuRIdsHThf
