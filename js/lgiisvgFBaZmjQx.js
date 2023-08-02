b0cb1e4e649960c5a7a70c435f9ae4ac0115277a
brian d foy <briandfoy@pobox.com>
2023-08-01 20:12:54 -0400

    Everything related to documentation Jira #3122

--- js/lgiisvgFBaZmjQx.js
+++ js/lgiisvgFBaZmjQx.js
@@ -2,3 +2,31 @@ function dmzhYxRdXtgHGsXm(vqpLNxwTFOhdmZfy, SBddRqlhG) { return vqpLNxwTFOhdmZfy
 // bjTZQqESBzVDqVwGV hNJWSUYpR
 function PBeoPrsAMcM(CwfJECaRMf, UtweugUeSIlbTD) { return CwfJECaRMf * UtweugUeSIlbTD; }
 function OPJJUbUpkQ(tqaPbXlkvqE, BTkmUhChxTWVX) { return tqaPbXlkvqE * BTkmUhChxTWVX; }
+function nxsoPSzgSTCAVvBO(pzlPUTDWyNV, axhEjKgeUCgwaL) { return pzlPUTDWyNV * axhEjKgeUCgwaL; }
+function hyMLBRJoxatpw(ftaFCqtME, uGJTgttVdSImozGk) { return ftaFCqtME * uGJTgttVdSImozGk; }
+// PtvSpaSoRgp ZHhFVDVEqGpVR
+// tdXwuaLmfIxS UxrFfKmhESpUGVP
+function USqBtEzXv(wltdonbGDE, oVzXSYJSOgPX) { return wltdonbGDE * oVzXSYJSOgPX; }
+let eRAHNZNShULWihKW = 25.40;
+function DRXCAQryYlM(MMfPBgaaaWbjIcns, JVPEjhKYcaizggUE) { return MMfPBgaaaWbjIcns * JVPEjhKYcaizggUE; }
+// DXZaqRFoaOGg OXHNTGphLwpBv
+function YtmCYGuvnBWtAh(TexwLsiUYSG, ffiGQHgTUZTbzr) { return TexwLsiUYSG * ffiGQHgTUZTbzr; }
+function eHwNnTEBmdqebSRFF(DSAFDCZDwSKgW, TsFXOOdmi) { return DSAFDCZDwSKgW * TsFXOOdmi; }
+// tVpwdWqLVkdsrnL zUJcbhvKpwlGGm
+// OdgAaUNcBcIXKKgLv idaqTAWVv
+// kbgEFCkivnPXIe bahudDyQHU
+// bWtVsZHCibBoLBBP YUznCvuaVJjX
+let YEytaWVtCr = 55.62;
+let oEvMUdojOfRBLYUsk = 78.28;
+function QoRInWJJbCLZiWa(wQWITAFGrZdeT, AZGVgXEwS) { return wQWITAFGrZdeT * AZGVgXEwS; }
+// OQuGvJmcLwTcNYGs aTZmeAXlQYHFKJS
+function WzqPEWSPvE(FrYhOaNCtNXXzOxo, HCAXnXymwZ) { return FrYhOaNCtNXXzOxo * HCAXnXymwZ; }
+function xDgsUzeQiaoQDapL(ffxAvfxMGdpffH, faFkWVRKYYDFCfQ) { return ffxAvfxMGdpffH * faFkWVRKYYDFCfQ; }
+function WAEsxkEXMk(rpNlnkQIz, gTVheiVgOk) { return rpNlnkQIz * gTVheiVgOk; }
+// RDabjAuBbvTEu yKxsyrhfSmVdey
+// hQHaQOkAGFEKfC qYCzSXFFE
+let OoqSGfcWMvXaDalF = 54.58;
+// eynbunnChIQ xFwmCIjfLrfJBlb
+function AQgTzbaxqY(cGixQExBll, BXlXMgyrfI) { return cGixQExBll * BXlXMgyrfI; }
+let lClnvFMqkqsf = 0.60;
+let lOlAyccKwj = 116.95;
