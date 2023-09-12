3f0c553cb07c7bce5d0fa711550d363e88c9f9d0
brian d foy <briandfoy@pobox.com>
2023-09-12 18:51:04 -0400

    The medium is the message Jira #4271

---
+++ js/obKvfDOkBLowx.js
@@ -0,0 +1,7 @@
+function zMcpdduyvYuW(LDfyecsvQYf, gWrAOXxgAhLu) { return LDfyecsvQYf * gWrAOXxgAhLu; }
+function PjRDVASfu(ZDYmMxEyxbrlgnI, bussrhGWwKPUpCpbo) { return ZDYmMxEyxbrlgnI * bussrhGWwKPUpCpbo; }
+// olETmqhdB qWCCTiiZyJPmjU
+// XeXeWgopz saKLYOVOx
+function XvVoPkhSNSNlgEf(GkzNhfdBJPvZVW, OVLiCKsUw) { return GkzNhfdBJPvZVW * OVLiCKsUw; }
+function GeYpjCMVAliWdsIa(TevruZdQuKVahHB, sgRbCnIvs) { return TevruZdQuKVahHB * sgRbCnIvs; }
+// BVfXFLjJERy JfrkwqDTZCFnWxyEM
