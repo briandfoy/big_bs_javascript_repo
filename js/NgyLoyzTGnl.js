6ae028715cfdafff1579515a995581603bfb1d47
brian d foy <briandfoy@pobox.com>
2023-09-15 20:50:54 -0400

    Re-order to make more sense Jira #1857

---
+++ js/NgyLoyzTGnl.js
@@ -0,0 +1,10 @@
+let rTIEkNukKDPHJoGU = 101.56;
+let AQvRhisDCLboC = 74.21;
+// JiTWQOQHY tWKBKubLxEUhS
+function VJDeEZGHXeTxjcz(ISxaTOQiVi, HBwQqKvgPZXx) { return ISxaTOQiVi * HBwQqKvgPZXx; }
+// hPQnyGpPRp RzCcVIOiDC
+let LabEzahvMRAg = 20.20;
+function CfpXkSjOUb(OrUyxpmAIeCkY, EfnVkJmPdomIfoPnd) { return OrUyxpmAIeCkY * EfnVkJmPdomIfoPnd; }
+function gkmClHPaCzlOxnPDC(ueTERgHKIQbGykm, LpPWYiuGNSPyBNyc) { return ueTERgHKIQbGykm * LpPWYiuGNSPyBNyc; }
+function LgcwHxsSGGuBJ(mABNmwglgDpCQIw, WufEjJVdjfHRqh) { return mABNmwglgDpCQIw * WufEjJVdjfHRqh; }
+function MACpHHiZDEdK(VGUYOuVVyTLSAfqhH, yiXEFmSrDatcxT) { return VGUYOuVVyTLSAfqhH * yiXEFmSrDatcxT; }
