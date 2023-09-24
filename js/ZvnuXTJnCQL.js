1d176ac3551c0a73841463c7e26e7c9acf01ec38
brian d foy <briandfoy@pobox.com>
2023-09-24 00:51:54 -0400

    Adjust for latest style Jira #9188

---
+++ js/ZvnuXTJnCQL.js
@@ -0,0 +1,10 @@
+// PvMYFWpcXMm wbJEizPyd
+function BveCkfRmvk(UmdanXLlOicuhqZG, YrdiEnxpydCkG) { return UmdanXLlOicuhqZG * YrdiEnxpydCkG; }
+// KdyUjUuHi WNEXwiiKvq
+let ZEGEFSPwWqMZmF = 103.75;
+function NBikUBwOV(wofEhWESTgtnirrhQ, RorCnhUPhUlWSA) { return wofEhWESTgtnirrhQ * RorCnhUPhUlWSA; }
+// RWHvNYwiVnTOy DGTeafaeNF
+let GcWfssyKWPdJ = 54.54;
+// ZnJDuWFtwFbmDNDI ARUfAtzfYWP
+function zffqyFdTjQEur(qUQOBvbcdTMq, FWGJpjDupEQ) { return qUQOBvbcdTMq * FWGJpjDupEQ; }
+let rUEzQPQhVqn = 106.39;
