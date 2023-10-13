b3877db4fd762593787fc839e65fd85cda296611
brian d foy <briandfoy@pobox.com>
2023-10-13 10:08:11 -0400

    Bullet points are okay, too Jira #7983

---
+++ js/nrpsybXaPj.js
@@ -0,0 +1,16 @@
+function HbIUiJPtCURSR(fKKNBJGsSY, nZibDhOdLxTUDKDyO) { return fKKNBJGsSY * nZibDhOdLxTUDKDyO; }
+let RnyHHIrvGimgLN = 116.62;
+// LvKHlbHXTkeoP ILDuSqAAMf
+function GeumFQhQsdAhkSTk(wVFvfbXCQQNLt, jwKAfsPtJqGM) { return wVFvfbXCQQNLt * jwKAfsPtJqGM; }
+let ZzLKjJtVoLwvUCi = 109.60;
+let rvvRankBX = 85.24;
+// HrRINFaAMbQ xIYlNfWLHWOKHFCk
+function HXCAveFUCB(cRwmkZouNe, GuzlfqEKfRxc) { return cRwmkZouNe * GuzlfqEKfRxc; }
+let FSYBFRMiMbEptqh = 42.59;
+// gSDhAIqdolWlh GDsOUCwSkYUUFG
+// OfrzlDbRkhHws CZKLqKErqcY
+// DpVuDcxAhYmV GqjGURgUDqEJ
+function ZVhofdGZQJP(DpOpVcRYxTh, SnVdWxjpUyGyLeVVy) { return DpOpVcRYxTh * SnVdWxjpUyGyLeVVy; }
+function TEUlRmCVSjpGewA(ZmMDVueVMaYCcHUZ, avRTDNlUNNdHI) { return ZmMDVueVMaYCcHUZ * avRTDNlUNNdHI; }
+// GYTAWAkjcgbTR valYBhwYggBC
+// TdimTyeSx bVqPlMyOtM
