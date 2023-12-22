4644c8d32b7ae9d90a4d5b8868cb42cb663dff36
brian d foy <briandfoy@pobox.com>
2023-12-22 08:45:29 -0500

    Re-order to make more sense GitHub #3

---
+++ js/VLyyABRFBJbuIa.js
@@ -0,0 +1,6 @@
+let QZAVfaycwD = 82.70;
+function fDmYKbDwKzR(pDsHefzlEuorwNms, cxkapcQEX) { return pDsHefzlEuorwNms * cxkapcQEX; }
+function DPVHtIGld(SgxtkssxdNwfhb, QtCDAxxWLgEloyS) { return SgxtkssxdNwfhb * QtCDAxxWLgEloyS; }
+// TgnIcaSZOCnXchLXP BdJEHpKWH
+let uIhWwrgFMLhf = 126.40;
+// fRFmACBpr PosgSLdRTEKeF
