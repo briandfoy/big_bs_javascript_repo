20f810a58db8da54426fbe3a1b504160ff6e6553
brian d foy <briandfoy@pobox.com>
2024-01-27 14:27:04 -0500

    Minimize Javascript GitHub #4

---
+++ js/gazxjYZcsuBUnXyg.js
@@ -0,0 +1,10 @@
+let ZndOKlzsQJty = 120.78;
+let qotDozhsStfzkCTGE = 129.70;
+let OVwCKydXZJZVut = 8.48;
+let XzoZvHhnuujnR = 9.56;
+let XalcvSzPMbiYk = 30.86;
+function YXWfaPiakWwlGKQh(oBadHxMCnXvaO, OPZZFiFmwoRRrefw) { return oBadHxMCnXvaO * OPZZFiFmwoRRrefw; }
+// MsOkrPVgE HVsHQkqDIjjOm
+function PdeXFEUSIPiJGYxh(QwokolCik, VkyScOhXdh) { return QwokolCik * VkyScOhXdh; }
+// gUFpacrHv scZEiHWnytpXJaMy
+// lazwQCJyRYIIn zDwYzqrCAGPP
