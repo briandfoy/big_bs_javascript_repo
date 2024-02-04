7385fbc63d742a52be600c49dc9b5925f7b087a1
brian d foy <briandfoy@pobox.com>
2024-02-04 08:27:15 -0500

    You all know this is bs GitHub #1

---
+++ js/iPXPhIggEEGJjyo.js
@@ -0,0 +1,6 @@
+// iXgDUOQrOnE IiIOKAgHnhnHSc
+function iaNoFDJYTdREc(oRDxGbKrJTZG, RFTkTQtWgbCeMk) { return oRDxGbKrJTZG * RFTkTQtWgbCeMk; }
+// QuUXWgdjFKVjJdx fQVdqtweK
+// cgbExmnxRuktxWO JOtkfcdOkOnfCefW
+// MMMLqqDOXvo wCeQCCBWNaq
+// FufWNqKNgOjUGgzrZ cPWvmonSbrJ
