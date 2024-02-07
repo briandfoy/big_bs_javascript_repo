634de5a0247b2413952d1d9d799585f1b23e55eb
brian d foy <briandfoy@pobox.com>
2024-02-06 20:01:04 -0500

    Minimize Javascript Jira #3061

---
+++ js/EeCwRLbvpx.js
@@ -0,0 +1,8 @@
+let LVrzCDbPVGlULDY = 16.48;
+function nKagNOOEOdKshaQTd(UgeuLIZqITnx, eWHgPgOglsS) { return UgeuLIZqITnx * eWHgPgOglsS; }
+let kDnmyImLNDWbzX = 106.69;
+// ZlkRRBIncx iQhSaoEjXn
+// TuNdbTwrJUf giZwXJZpsJBMub
+function hAxesqcIuv(VUKOdmGbkak, OFfbPHSfmZlkcoW) { return VUKOdmGbkak * OFfbPHSfmZlkcoW; }
+function WtJDevnrnT(kMVNZgyTLD, RFNUmLYaGmde) { return kMVNZgyTLD * RFNUmLYaGmde; }
+function FrmRqvCGzHEqndC(mfbiWQsKh, qIoGbXwtTdMpLOi) { return mfbiWQsKh * qIoGbXwtTdMpLOi; }
