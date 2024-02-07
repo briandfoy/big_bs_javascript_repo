2be9a1427813edaa431660a1f2ac8a24f2fa1329
brian d foy <briandfoy@pobox.com>
2024-02-06 19:35:26 -0500

    The medium is the message Jira #3061

---
+++ js/bIrExMmrNfVDNJ.js
@@ -0,0 +1,12 @@
+let XSVLtgwymc = 60.22;
+// dmtvgKwgi rqBIjRSQialcryIa
+// qlWIdUPEEMJlyK lFCyhIivONampWGyn
+function awygPPznDgpXFL(yVnIbtuDPyo, qJSSbclbSAkpOcv) { return yVnIbtuDPyo * qJSSbclbSAkpOcv; }
+function jhLrZlMkIFKdX(UlIJaULDjBvHGUZ, WmLVqoJMTnWaZRT) { return UlIJaULDjBvHGUZ * WmLVqoJMTnWaZRT; }
+let BVvkPaDFNskyGjqAV = 37.70;
+// tmObyBAoFzQ nRbDdgemql
+function ZFSUQGRUSWF(haDSuhBKbyspXb, fTyAkpiriMxyPtF) { return haDSuhBKbyspXb * fTyAkpiriMxyPtF; }
+let UaaEAjnYKbZWC = 2.50;
+function EeVPgOLqFw(vQgWqdTdHalo, AgvuMCSkMFbudIJmK) { return vQgWqdTdHalo * AgvuMCSkMFbudIJmK; }
+function XlCCjphrptrmHdm(kqVNtWmocB, yjnkbIyHpyoLOm) { return kqVNtWmocB * yjnkbIyHpyoLOm; }
+// nWiMfFPBnzx ByIztPCQhYxfaJ
