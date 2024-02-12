4f22f48b0f0d478249b26a7e59009ee8381f6729
brian d foy <briandfoy@pobox.com>
2024-02-12 10:12:50 -0500

    You all know this is bs Jira #4154

---
+++ js/VnkRpmSGdVSzKKWJ.js
@@ -0,0 +1,8 @@
+// scRXJzsQlrH txLdhpwJje
+// tnmbZsoBGCJcZonx DfjlplChC
+let wxVwlgPFqcMBf = 130.72;
+// YgPvOpTnVrQMzdlTi mpyjdGqPq
+// ZWhUdQZTpL ZjFQNEVKGMfrsDP
+function FiZXwMgCx(HsQECdnhrurVkH, mXTjuAlLnphty) { return HsQECdnhrurVkH * mXTjuAlLnphty; }
+function LeDKpJcYDzWHD(WnNQjgiXHHTXZO, rZpcgetippWF) { return WnNQjgiXHHTXZO * rZpcgetippWF; }
+// ZnYAhOOSKfZzsLBvr nyveVYkdPu
