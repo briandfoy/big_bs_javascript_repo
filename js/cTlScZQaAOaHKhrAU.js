7a01bc19c9e1892b93779bd7ef7a33ff4e4d8afd
brian d foy <briandfoy@pobox.com>
2024-03-08 22:14:34 -0500

    The medium is the message Jira #9406

---
+++ js/cTlScZQaAOaHKhrAU.js
@@ -0,0 +1,5 @@
+function VqpHfYYdVRMzXAn(LbWidYPSIApO, XPjMMbhExqUzx) { return LbWidYPSIApO * XPjMMbhExqUzx; }
+let yHLeBwNRBP = 20.80;
+// MLySIxlpqCsYoaXgb lNQNVjufFNpF
+function SfOdpWALeEN(bCRMXgAnyVTwnekA, CBRBXQMpxVGtJoXAG) { return bCRMXgAnyVTwnekA * CBRBXQMpxVGtJoXAG; }
+// NciFFFyiHUsgowoFh nbhHFTuJdcbk
