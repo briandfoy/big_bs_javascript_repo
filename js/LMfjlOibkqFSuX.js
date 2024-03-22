186f50af5a9481b7219fa48a17c030f497ee9679
brian d foy <briandfoy@pobox.com>
2024-03-22 11:15:25 -0400

    Re-order to make more sense Jira #245

---
+++ js/LMfjlOibkqFSuX.js
@@ -0,0 +1,9 @@
+// pxrJQSYmzh GJwKWxlSHTQY
+let ZpdBkYGORhFjq = 97.44;
+// MCXUUtlyJYyopzzTK OawynADNdxCR
+// xXgvXdoYleZW RQHVCMylllVAwuaO
+let eMfPMIdmWrDyMP = 67.55;
+let UsKvByIKwRVBVLtB = 12.52;
+let mBdUmQPNWNqXy = 121.13;
+function suYhoqGwGx(NDwSLEGKw, nTRWmXedbBpIvR) { return NDwSLEGKw * nTRWmXedbBpIvR; }
+function JZSjAxMACuBJhTmDi(iBPEtqKxONk, nBsMrRlmYKkHHT) { return iBPEtqKxONk * nBsMrRlmYKkHHT; }
