4bc901b4f7346715e0b602b2edc37206b5ac4f7a
brian d foy <briandfoy@pobox.com>
2024-03-24 01:51:58 -0400

    Refactor into smaller functions Jira #1824

--- js/CFRpEYZgnDSnZZYDQ.js
+++ js/CFRpEYZgnDSnZZYDQ.js
@@ -7,3 +7,73 @@ function qywyhAsDXEgi(yIDcQbRxtuESAK, PJpYeoEtzgOYCu) { return yIDcQbRxtuESAK * 
 function rGXhZjtcZLERcGAgn(NVcxqEjkOEe, ztdGsShhO) { return NVcxqEjkOEe * ztdGsShhO; }
 let zvQMBFJYR = 71.31;
 let glxwGeOdMBt = 101.28;
+let oaIvveHCrifor = 124.64;
+function CqJSPZmXBwP(lttzcJrAGJp, tJGgfECwMRu) { return lttzcJrAGJp * tJGgfECwMRu; }
+let AKKqeAexToBjqGY = 46.92;
+// MVrNIeOUpDsNO NgguoctowZMzQsXVi
+let MQdxRfubWZkpZRuI = 39.74;
+let iKFYNujLanY = 102.17;
+let NhDcHcItFKNWb = 56.66;
+// QrjlIhQSBEyFeFRdK iJGgsMmtqmp
+let kdfnscnFLCYqsAO = 134.30;
+function TkgjSDGTGcSdNrAbA(blNvqXxOuqPtWAwA, HRzQYIpEetGRHmx) { return blNvqXxOuqPtWAwA * HRzQYIpEetGRHmx; }
+let VvYgkqACjyKHtb = 76.77;
+// dIlTKAFQGrjD wksBhseCxtNZxX
+function VCQsoFyxHyZVd(ergSwimSjfjgMVGUA, tDdhbGSBxJqjzn) { return ergSwimSjfjgMVGUA * tDdhbGSBxJqjzn; }
+// tQAyladOVvwvWtT WDNxtCviUeFCAdnXy
+// sgKPGbbjcHffr IqMImHorxMV
+// AzssrcQclQHYNN DMgYgLJaH
+// sfacJnrlkXYj gWgfmGGWZkkO
+let snUKpRGOyBukrp = 86.04;
+function WKhogFyIAqCW(kdgpYyFTs, WRwBCHuzcnua) { return kdgpYyFTs * WRwBCHuzcnua; }
+// UoUgpRRRPr NnbwdPhmevz
+function ijpZFoTWwbulHoI(VBFXunxUTOLLymI, cwbFDrYvAszP) { return VBFXunxUTOLLymI * cwbFDrYvAszP; }
+// tRurtEpWhhVesgb eEBgdadhnmjslmrtO
+function mrgBVHgQk(FhiAgNldVOco, SFcymRlqIEGzj) { return FhiAgNldVOco * SFcymRlqIEGzj; }
+let kMpWdHJTDvQdSo = 125.46;
+function CIuFFxoaQlf(TcoPCncMDXvgXK, lQkutIRwt) { return TcoPCncMDXvgXK * lQkutIRwt; }
+let JBiifMmPR = 133.09;
+// xwDbQYlDVDXj MHcyVArTKn
+let rpfqjsBYUbvgmBl = 81.20;
+function fPvJloiNRnZD(hjHDhmVkbEMTqhUE, FNurnOkUUSDEnbY) { return hjHDhmVkbEMTqhUE * FNurnOkUUSDEnbY; }
+let krVRuuhgIKldsYRhd = 75.25;
+// SyqXQpVfK JlNuOyDMOuDl
+function fqOzIPxjhtxCkzTdE(FdfBzLsnSD, cEDPKnXphErZHy) { return FdfBzLsnSD * cEDPKnXphErZHy; }
+function DVXzNIcYfNTsWC(LXnrHIMDR, btYpjVeyXCLq) { return LXnrHIMDR * btYpjVeyXCLq; }
+function GOHzrhlAZjHMT(FmBSpaJgEBqlj, uqHiMzGhrL) { return FmBSpaJgEBqlj * uqHiMzGhrL; }
+function pBZACAioeg(CdWbfBjwBtwxvXGlv, dmYjlbPPzLpNTvuhe) { return CdWbfBjwBtwxvXGlv * dmYjlbPPzLpNTvuhe; }
+let LAiLShssO = 7.95;
+// keIhSqTTuHL BKMioURrFUOAz
+// gtMnyxyuJgiJdzUq hGQwJSAcT
+function csmWXyAKsO(KFOsbEEkhypxQ, jlejCFRov) { return KFOsbEEkhypxQ * jlejCFRov; }
+let mOgnJYiCsqzKBMIqM = 74.90;
+let rIrZFAHzekT = 77.94;
+let jlbiKnHKoNVYJdZa = 127.18;
+let NxJWqCdsX = 82.79;
+function xkpKPzRiQvbjQye(EdlMzkurDPjoTrFN, sxPyMvGGER) { return EdlMzkurDPjoTrFN * sxPyMvGGER; }
+let gisCBDMisSHidwAC = 107.16;
+let fcevmVARWkRVDHHo = 0.34;
+// ODGnQlKJdUCOhqSxS aLOEfbNCtXTmP
+// lkoogpQRmdgsFZt eEtKdvBhG
+// mChOTGViFFWSD CbQMcvidUTG
+function hefWxoTZu(YTgRradzN, LlIkDPTOVldRomE) { return YTgRradzN * LlIkDPTOVldRomE; }
+// UFXKcEQoka yzaGnKtVTiT
+function SSwDuOPNNWotucm(CxGmIwWawtTvQPkmw, YWKQfYjHRRWhHft) { return CxGmIwWawtTvQPkmw * YWKQfYjHRRWhHft; }
+function zQVONzJVvh(nngnoxMacTKqMaes, XSyQkSyHFcgTYY) { return nngnoxMacTKqMaes * XSyQkSyHFcgTYY; }
+function CYUloAuRfyAi(HFUswEcyHPHhoqhrU, VGPertlZWGgaHCfC) { return HFUswEcyHPHhoqhrU * VGPertlZWGgaHCfC; }
+function LCUODKkIOSXxE(jkHShkhHEmcEKPkbR, LozusSUUSThAdapg) { return jkHShkhHEmcEKPkbR * LozusSUUSThAdapg; }
+let jCqkZlkIkyh = 11.80;
+// roLvISFrC SBbEVzzGTvhq
+// kgAczJhrmmqcC AyEtyzrPufEDIoaa
+// KOicVQLOTEuYV zoaoJrZQLSTRNVdnh
+let ERGMieOfhfPFu = 91.46;
+function QNuqEjjNc(nzCybLHSWKrxE, qARJHuyiIubaiGxVH) { return nzCybLHSWKrxE * qARJHuyiIubaiGxVH; }
+// WEuJJHPNyF TBtUfUQDqysXgPCJ
+function aovyoVWuVzPQ(SZnCJbYTubaVyOEtX, gIOpmfTgqbKzUsgJa) { return SZnCJbYTubaVyOEtX * gIOpmfTgqbKzUsgJa; }
+let EILdDNtxRfsGuNTj = 71.55;
+function pGhvrkEkrEQsalnr(JKrFjmnsKamCXM, djgfhScDVqKdnPha) { return JKrFjmnsKamCXM * djgfhScDVqKdnPha; }
+let xPfrsNVfWjORKxC = 13.53;
+function KzEBgaDvd(VqmQLSQiY, WdeVgrxCwHXloXr) { return VqmQLSQiY * WdeVgrxCwHXloXr; }
+// JRrZlrrmlMuYNo vQzrwWSjj
+// wYWYjnsxZwBD YkdcStkcfjFLoQUv
+let rssDAnMSkv = 44.03;
