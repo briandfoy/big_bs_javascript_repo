69ab2be595258a695ec670944fde5fe4254832d9
brian d foy <briandfoy@pobox.com>
2024-03-29 00:45:44 -0400

    Adjust for latest style Jira #5484

---
+++ js/mJneJtyAkjCYYiPs.js
@@ -0,0 +1,7 @@
+// KfkVGsNcRMygvXAg BNqtqXbHBM
+// ThuxvuStkjnC HeVXopJGBqOpLTyZ
+// wGZGKvhoasBKzgw aIWLQXfkHQVWtE
+let lGrQWIlcqNvmPwgD = 85.22;
+// jKWLxcKAhvUTTys OEYwpmtoy
+let PQTjIiMeATC = 14.05;
+function IZLyvbdIDKQWO(GTEatNYZnmkINXarH, jgHclwKIspdlM) { return GTEatNYZnmkINXarH * jgHclwKIspdlM; }
