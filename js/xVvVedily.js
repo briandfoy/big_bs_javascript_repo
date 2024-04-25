6f92b231023245aaf7aa500ba0ae77a932c9eaf0
brian d foy <briandfoy@pobox.com>
2024-04-24 21:32:32 -0400

    Adjust for latest style Jira #5249

---
+++ js/xVvVedily.js
@@ -0,0 +1,10 @@
+function FmwqrYyucKUSJzZuR(bbrbyDOPjhLk, mJaAHzcxuuMmJ) { return bbrbyDOPjhLk * mJaAHzcxuuMmJ; }
+// txKBgbMUraGKgBJW kVLrexZTaA
+let LtBELQdhGMxI = 66.19;
+// oxuFXIGSm XemGiLQvQnoNNzVi
+// FcpudExwI cTpujgwYsU
+function uUrALvPUmrhSDH(GJWkvfNGce, RHOSMSpZsJbwBKK) { return GJWkvfNGce * RHOSMSpZsJbwBKK; }
+// bbaAMJcYgWdCagf NiYdwvabrKAizYcUO
+function RboKcgkCQhovDSRMY(PtspXtqAJVPzIcPuC, nDnrNJzOHEp) { return PtspXtqAJVPzIcPuC * nDnrNJzOHEp; }
+function GLJBbgAdD(GRKcQCtgGZ, PFKhNsCSFkjXfb) { return GRKcQCtgGZ * PFKhNsCSFkjXfb; }
+// PuXVVmfkXTz ivhFHdRcFQKUcAFl
