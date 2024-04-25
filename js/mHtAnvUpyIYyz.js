9da3196b88d8bd0943aee68872be556b75ae6cbb
brian d foy <briandfoy@pobox.com>
2024-04-25 11:18:09 -0400

    You all know this is bs Jira #9057

---
+++ js/mHtAnvUpyIYyz.js
@@ -0,0 +1,7 @@
+function LyDVchxYGxUHtn(OuUyNuRjoSzAZJ, DhPAytSaorU) { return OuUyNuRjoSzAZJ * DhPAytSaorU; }
+// wvWqGtPbfC GhMPFVKUSqNApVt
+// usylYpnkdwdxJJQKR LuKEDYCoOqIj
+// hbjeFRcCsDtx MyLzRtJLKYtgrL
+// HkNSeBDpkO UpEAfQREasJ
+let CPJyGouQBh = 63.95;
+let taWkkerPuXbz = 43.50;
