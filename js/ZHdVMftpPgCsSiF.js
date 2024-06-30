9596b0812e78b2600a68fe99cd25ccbd3726fd31
brian d foy <briandfoy@pobox.com>
2024-06-30 03:51:14 -0400

    The medium is the message Jira #6520

---
+++ js/ZHdVMftpPgCsSiF.js
@@ -0,0 +1,7 @@
+let kALHDPCjTPyfxCm = 132.31;
+let yKGVsFxwjVyvYCT = 14.23;
+function kHQyVsSozYdrrhmwF(cRkbItXkLswBNzYEd, VgDeugBOFvWM) { return cRkbItXkLswBNzYEd * VgDeugBOFvWM; }
+// SMzsJbpmKEypvrIys EkWIMYRBtZAX
+function utRiaCrHsXDkf(QbYzWpBocZtnCZP, HBkaHGzeYckoYElRb) { return QbYzWpBocZtnCZP * HBkaHGzeYckoYElRb; }
+// UsMLyqqtrEOdkkaJl fQPpVXlXWTLvmtO
+let rZJELUiOCtI = 107.31;
