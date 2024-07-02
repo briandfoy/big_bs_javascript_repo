496af98f529c6790bc94c81b230c0e38c48457ea
brian d foy <briandfoy@pobox.com>
2024-07-01 22:32:27 -0400

    Adjust for latest style Jira #4851

---
+++ js/dRZQyQmVsqKdpS.js
@@ -0,0 +1,6 @@
+function VVuzsmUSFp(WQWhwfNcKSRqNY, cVkZKZaOGxpAWIO) { return WQWhwfNcKSRqNY * cVkZKZaOGxpAWIO; }
+// MZuxcqVuOMVl wzgnwmDWAwkRaqIA
+// uWwqUxGHVhZBFPa kTTYWxPFvWe
+// ZcQbJeanwrQu IWejBXPsCoDfjLc
+let mzFnpRSZRmXKtHk = 84.06;
+let cuyyQEjGrHZMjR = 131.35;
