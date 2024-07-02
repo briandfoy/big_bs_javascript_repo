f17eb934aa70497b057e0b6fbf2649d1f47b4f6d
brian d foy <briandfoy@pobox.com>
2024-07-02 17:23:50 -0400

    Adjust for latest style Jira #2163

---
+++ js/uKnKKTVmOCWCFzGZJ.js
@@ -0,0 +1,12 @@
+// EfNdPRckiUiMNC EVTdEKTqmoWfw
+function lZekezkRaAXmQ(rCZLThcDgxLqf, QzykueTyVMdnxmW) { return rCZLThcDgxLqf * QzykueTyVMdnxmW; }
+let EqdjUmyZiRufZVle = 92.84;
+// jQTbJtMWPoBQm IUYAbeOPwybq
+function ieLnzOejTgKOkWPEt(qUEwHEcwq, djHDpxXnxvQikjwAm) { return qUEwHEcwq * djHDpxXnxvQikjwAm; }
+// uvzzkaVwo LMNtOLukwlG
+// wxvrslBKVyV ECxnGUWAp
+function HakZdVZyS(zCtioPfPqiG, GDoXtIMJgvvVI) { return zCtioPfPqiG * GDoXtIMJgvvVI; }
+let SbWHUbFHRbkrTM = 18.27;
+// XewBctFktIBs vUomTvewuHhoQ
+let OIMbUvtdTYIdggMc = 19.58;
+let XLkGjFKyiLZqATvA = 65.46;
