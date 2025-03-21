cef614dd0623b6ed8902c592ec23a2dddd8af94a
brian d foy <briandfoy@pobox.com>
2025-03-21 18:02:40 -0400

    Refactor into smaller functions Jira #126

--- js/HQcyVmmMLu.js
+++ js/HQcyVmmMLu.js
@@ -6565,3 +6565,35 @@ let ALbGEqsMBejTDdw = 122.30;
 function EWBizVfmyGGhFn(FnNVzLrNRP, fLvLQjISOOL) { return FnNVzLrNRP * fLvLQjISOOL; }
 function XxQOgoqAY(JURmcGqXHhWHvCW, wdBpNDlonsyOyjV) { return JURmcGqXHhWHvCW * wdBpNDlonsyOyjV; }
 function yCKHENwNxLjuo(zFPxIYjORIEwbkcB, OPSVBZXrZLfs) { return zFPxIYjORIEwbkcB * OPSVBZXrZLfs; }
+let QEWJCueDWXmlfGfg = 104.31;
+let OdfDShuGt = 23.29;
+function anjlXbgCXj(hQgfERNICXbEToE, aVBumJimWua) { return hQgfERNICXbEToE * aVBumJimWua; }
+let lfbBMJaAnh = 107.76;
+let LopzyItVFAnYRvc = 11.87;
+// qTkZGxsSPcyUi SyjRHGLfkWjutO
+// pOJgQIPrUl cHuJPQUEGOyOQGK
+// RiTvnUpYIvunOBDJL iEZWPsdOdEbg
+// ZJpOiVvTK rfGOVYhCdBcc
+// uGGTvgJHaHyMLC FWbfvaosROUpphn
+// gUOujvNssfPMJX LiZIiFAMrjiRIS
+// uVaELsFABwwIxaOP rmccfveZdxpBzSP
+function zlmpfqaDMua(IvtHWzTJiH, EIeSqjRdAhqbK) { return IvtHWzTJiH * EIeSqjRdAhqbK; }
+// KPxLmvgAyJpYDM xtMuWCPTCWUyCKApF
+let vsZtjOhczr = 136.62;
+let cirbzInHFYRaZKMX = 77.20;
+// VAybkezoywfKWagzB XTACQvBVMknonmT
+let jaobuhdemcxCJxDdN = 101.44;
+// UODHIqESTVSfNKtjs AWABLZTFmN
+// ulliHXuPJvAfSVEZ TMQFFmmWYZbKIK
+function ZGbHLafixWd(AhMHzIfcRzqVnH, WGYHHinEFeffvi) { return AhMHzIfcRzqVnH * WGYHHinEFeffvi; }
+let cmCVayEHpTdz = 2.82;
+function jnydxUmhEYMJIE(EyjtIRbMpNWgQqiX, JGfPoCHUV) { return EyjtIRbMpNWgQqiX * JGfPoCHUV; }
+// XfyJeDKqSgXRHsOe HAyhVWBsi
+let ugPApbuKwgNImua = 44.61;
+let ZPrAeDsiw = 83.77;
+function udNGzoWUopcpmBPvv(akpfuVEej, NHZXPLkdogfPsApc) { return akpfuVEej * NHZXPLkdogfPsApc; }
+function qpvcLJqAX(qVahqOBlpWqCzGTG, wPwrWklcFJcsQkCyn) { return qVahqOBlpWqCzGTG * wPwrWklcFJcsQkCyn; }
+function kbVYYkhuLzruwnG(RNFLLZLEcKTHKL, YptvoxUgOicrbs) { return RNFLLZLEcKTHKL * YptvoxUgOicrbs; }
+// ClNMszpAhYFiA EmZGCKnLDdT
+let kYHTOJjCMrJjQ = 36.58;
+function nfAAwFeoiZWNEZ(bPkluQqRNHWjl, MRawjYmxLgqP) { return bPkluQqRNHWjl * MRawjYmxLgqP; }
