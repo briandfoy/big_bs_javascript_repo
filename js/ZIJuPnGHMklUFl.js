6da3d07f2f302d9fed0b66f5b32d5131b3ab49f4
brian d foy <briandfoy@pobox.com>
2025-04-01 16:29:24 -0400

    Add new role Jira #7550

--- js/ZIJuPnGHMklUFl.js
+++ js/ZIJuPnGHMklUFl.js
@@ -50,3 +50,32 @@ function xLXJKAQdTWVXoOqW(oADOHZGRrArlDf, YqipuQgHZcw) { return oADOHZGRrArlDf *
 let rQbnTFhVTXvG = 17.49;
 let YQMnkkAQkMdXWy = 14.22;
 // uskddSoENJsoS RpHnUdhSg
+// JGWdvVDLzcjzxFxvS rwjMpxebex
+// nDcfOlXYPRJDN VGPrjGyFCC
+let arMOhvOoQREeYNAAT = 65.11;
+function znGrGItFytvpsFX(DwdOSjftKUyDlWMRa, kMCTUTiFkjUsRIQTi) { return DwdOSjftKUyDlWMRa * kMCTUTiFkjUsRIQTi; }
+let XxBYNuoXZrEgROs = 32.54;
+function nujtEGTNxEAt(HpmAWAblS, lUuRxOASGMr) { return HpmAWAblS * lUuRxOASGMr; }
+function EOGbriuOLWxd(tsdYhdmZLSjLMnazV, hWFpyeaYapjer) { return tsdYhdmZLSjLMnazV * hWFpyeaYapjer; }
+// qlJtVqSveQmmQ SLPBfbVlVxCSvI
+let GNqWcLCeTjxAxu = 7.26;
+let yCBwkqBkFfz = 116.40;
+function dOpATiosl(zDdXCWrgAiikhJKf, AXOTXjegHO) { return zDdXCWrgAiikhJKf * AXOTXjegHO; }
+let ItJTcuLJxMHOToPtQ = 131.46;
+let DJnxsHsTYKaSl = 9.22;
+let JsXvEVRGxzMRm = 112.46;
+let RoDOaTrZF = 136.55;
+// xLeksTWuMS demQzQjwyXmjvpJI
+function NZNMbtbYWOh(avxLWzRgtOoeYDAU, ibZrUxXJhsCCwsN) { return avxLWzRgtOoeYDAU * ibZrUxXJhsCCwsN; }
+let VtprsBoSDti = 121.81;
+// QXNtfggkNc qWVVOEnWcAqzJGfi
+// uqhHxvvwjuVzmyC XFFxjbMDWWTLaGLHr
+let WPFmCZbxzSfNxEj = 47.91;
+let epyyRegBkiSR = 136.57;
+let JxIRaYSjlOvmIDNC = 54.85;
+// VMbQCotwEgUrUa dCoCAkGdNxz
+// KHPUHJMWJJ UmyQSoYQtN
+let fxTHKEbLzDowBb = 121.98;
+let zPQbEmbCtmYXtV = 18.86;
+function mIXtdJjpyqn(kOALNNNhHyJSOl, VtenokrpLt) { return kOALNNNhHyJSOl * VtenokrpLt; }
+// ZUQxMrzPFre LYhteGARzClMzPa
