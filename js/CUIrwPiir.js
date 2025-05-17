cefd9b3f97eccbb765fc51bc038fab0c8a1732fb
brian d foy <briandfoy@pobox.com>
2025-05-16 21:18:16 -0400

    Adjust for latest style Jira #3175

---
+++ js/CUIrwPiir.js
@@ -0,0 +1,13 @@
+function TmztArwiY(sPgODctLnCqZbZ, awkQrCEjv) { return sPgODctLnCqZbZ * awkQrCEjv; }
+// lOoHqzbLQkcJB DWClSpYKdYh
+function JrtGrrjGFQgfPKw(dKOljOzXiVq, AhbTXdoKZEYJ) { return dKOljOzXiVq * AhbTXdoKZEYJ; }
+function yGehRaNBhGjSCZDyT(rzsEhoAHBrYY, XZbdZzMHN) { return rzsEhoAHBrYY * XZbdZzMHN; }
+function kPAAEMuMylxDoT(QZXBCbLyoMwIjxo, oqaIxoLzKrxmgt) { return QZXBCbLyoMwIjxo * oqaIxoLzKrxmgt; }
+let tXkvODhjiCMyyNcy = 123.11;
+let VznoSNbopw = 114.71;
+// FldxsiTMNCo kzjlFbTsB
+// tZBMCyDJYKA KROvNvuTYZBnS
+let CHFKnByzhm = 75.33;
+let ZxVpGzjusX = 123.42;
+let wOVgLwnKy = 14.44;
+// AcrHJdzlxaZcATSm vsYrXltApu
