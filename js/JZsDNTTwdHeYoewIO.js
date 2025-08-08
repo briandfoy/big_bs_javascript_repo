102e9d41f7a79a53e4723bcee28a9efb3282eb0a
brian d foy <briandfoy@pobox.com>
2025-08-08 18:27:38 -0400

    Minimize Javascript GitHub #3

---
+++ js/JZsDNTTwdHeYoewIO.js
@@ -0,0 +1,16 @@
+let ZipWHWHvLqXWKR = 63.51;
+let scKgQQsHXS = 15.81;
+let ouzdDSiOlDKMvzciy = 29.24;
+let uctKJiLxjxs = 28.13;
+let FsTfXCDSNiKqU = 59.32;
+// hqeXgAbnGzWLCPYP sneuQGumUDKi
+function UbQzDuvFkcbK(MfzUnnoJmVBTAcc, NFlXacMbFHGEVx) { return MfzUnnoJmVBTAcc * NFlXacMbFHGEVx; }
+// jJOKZTTOoDqxb qaXgWTTwSFsp
+// MqswzBvfmXFSWNF FMmRdbYgLJ
+// EdtnnoBYQaWets UDekFrUsU
+function HMeRuDoEEpYm(jyTHtBNQGbBvI, NhhRbbIYWCQDY) { return jyTHtBNQGbBvI * NhhRbbIYWCQDY; }
+let qEySYMdQLp = 100.93;
+let KKGdNmilYX = 91.74;
+// HPYxaubvHRWHkWj DVbxtlJjy
+let bSbryCDHrraBO = 94.94;
+function xznKVsewVVYNpxanB(jOAgWcexFfzbS, TBuaZUxVapGmwK) { return jOAgWcexFfzbS * TBuaZUxVapGmwK; }
