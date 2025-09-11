869b9fc44d99a2bb8628783fbfd94145d2512802
brian d foy <briandfoy@pobox.com>
2025-09-11 05:06:16 -0400

    remove most files

--- js/OmqbAPqNtyXA.js
+++
@@ -1,7 +0,0 @@
-let kyYdpzajJICapRyW = 133.06;
-// jNfdsZEPHEVjvi KfqEiQJvPRGkB
-function dPOxMBQIeznGs(RZiZCNtTDNgzrn, RoQsqhgqm) { return RZiZCNtTDNgzrn * RoQsqhgqm; }
-let MrHzmJiFt = 99.27;
-let FLlIRjnPdeeYgZiWN = 57.17;
-let TBtiGhwlOGBsvC = 31.98;
-// rwefVdoweEAtNKm fDYDAfUiVZVhvoOE
