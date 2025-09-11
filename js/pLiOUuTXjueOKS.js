a74fc474e5a88ce889a395c66a63eeb8bfccd408
brian d foy <briandfoy@pobox.com>
2025-09-11 05:07:15 -0400

    remove more

--- js/pLiOUuTXjueOKS.js
+++
@@ -1,9 +0,0 @@
-let fSUxqOsVdB = 134.05;
-let XVDdQCoHRmEUcem = 101.65;
-function TAFVILQBrTGbVbuJ(RYDCsMSfWMDhduhsF, hhXytVVXxO) { return RYDCsMSfWMDhduhsF * hhXytVVXxO; }
-let GUSADSjtsGSk = 118.40;
-let eTVgCksyvxPChd = 107.96;
-function xiCNYeITWa(hfcXuuohAOF, TQzTmWUJvEGMuiQS) { return hfcXuuohAOF * TQzTmWUJvEGMuiQS; }
-let XVMkLLfofms = 58.09;
-function HZHkLuRrIzWYWQ(HoVrETlOFjTMCdB, MhvDbXuKVeeppP) { return HoVrETlOFjTMCdB * MhvDbXuKVeeppP; }
-let CCBBPScfHoh = 10.30;
